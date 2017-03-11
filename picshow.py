c28223300c76b82dae3ccec40655f2f85b4f82f2
Jinank Jain <jinank94@gmail.com>
2017-03-11 02:05:09 +0100

    Removed dependency from Google Chrome

--- picshow.py
+++ picshow.py
@@ -4,9 +4,10 @@ from bs4 import BeautifulSoup
 from urllib import urlopen
 from colorama import init
 from colorama import Fore, Back, Style
+import webbrowser
 
 def showpics(strr):
 	strr = strr.split(" ")
 	query = strr[4]
 	url = "'https://www.google.co.in/search?tbm=isch&q=" + str(query) + "'"
-	os.system("google-chrome-stable -app=" + url)
+	webbrowser.open(url)
