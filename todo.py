9916d2c662cc0202f0102955e186b9d5eedf4810
x1ah <x1ahgxq@gmail.com>
2017-03-13 23:55:13 +0800

    add python3 support PEP8 support, code more Pythonic webbrowser instead of os.system

--- todo.py
+++ todo.py
@@ -1,65 +1,67 @@
 import os
+
 from colorama import init
 from colorama import Fore, Back, Style
 
 def writeToFile(l1,l2):
-    f = open("todolist.txt", "w+")
-    for l in l1:
-        f.write("%s@@@@1\n"%(l))
+    with open("todolist.txt", "w+") as f:
+        for l in l1:
+            f.write("{}@@@@1\n".format(l))
 
-    for l in l2:
-        f.write("%s@@@@0\n" % (l))
+        for l in l2:
+            f.write("{}@@@@0\n".format(l))
 
 
 def _print(l1,l2):
     print("Pending => ")
     x = 0
     for l in l1:
-        print("[%d] %s"%((x,l)))
+        print("[{0}] {1}".format(x, l))
         x += 1
     print("Completed => ")
     for l in l2:
-        print("[%d] %s" % ((x, l)))
+        print("[{0}] {1}".format(x, l))
         x += 1
     print(Fore.RED +"Commands Avaliable {add <todo>, remove <index>, move<index>}"+Fore.RESET)
 
 def todoHandler(data):
-    f = open("todolist.txt", "r+")
-    lines = f.readlines()
-    f.close()
+    with open("todolist.txt", "r+") as f:
+        lines = f.readlines()
     completed_list = []
     notcompleted_list = []
     for l in lines:
         x,y = l.split("@@@@")
-        if(y[0]=='1'):completed_list.append(x)
+        if(y[0]=='1'): completed_list.append(x)
         else : notcompleted_list.append(x)
     _print(completed_list,notcompleted_list)
 
     while 1:
-        inp = raw_input()
-        temp = inp.split(" ",1)
+        try:
+            inp = raw_input()
+        except:
+            inp = input()
+        temp = inp.split(" ", 1)
         c = temp[0]
-        s = "0"
-        if(len(temp)>1):s = temp[1]
-        if(c=="add"):
+        s = temp[1] if len(temp) > 1 else "0"
+        if(c == "add"):
             completed_list.append(s)
-        elif(c=="remove"):
+        elif(c == "remove"):
             x = int(s)
             if(x<0 or x>=len(completed_list)+len(notcompleted_list)):
                 print("no such todo")
                 continue
-            if(x<len(completed_list)):
+            if(x < len(completed_list)):
                 completed_list.remove(completed_list[x])
             else:
                 x -= len(completed_list)
                 notcompleted_list.remove(notcompleted_list[x])
 
-        elif(c=="move"):
+        elif(c == "move"):
             x = int(s)
             if(x<0 or x>=len(completed_list)+len(notcompleted_list)):
                 print("no such todo")
                 continue
-            if(x<len(completed_list)):
+            if(x < len(completed_list)):
                 p = completed_list[x]
                 completed_list.remove(p)
                 notcompleted_list.append(p)
@@ -68,7 +70,7 @@ def todoHandler(data):
                 p = notcompleted_list[x]
                 notcompleted_list.remove(p)
                 completed_list.append(p)
-        elif(c=="exit"):
+        elif(c == "exit"):
             break;
         else:
             print("No such command")
@@ -77,4 +79,7 @@ def todoHandler(data):
         _print(completed_list,notcompleted_list)
         writeToFile(completed_list,notcompleted_list)
 
-os.system("touch todolist.txt")
+
+if not os.path.exists("todolist.txt"):
+    with open("todolist.txt", 'w'):
+        pass
