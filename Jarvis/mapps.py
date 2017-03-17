2b68929bd5ab293f6879b0139a2bddf3750cd359
Lucas Keune <lucas.keune@rwth-aachen.de>
2017-03-17 16:37:58 +0100

    Expand capability of mapps.directions and mapps.weather
    Add 'from' and 'to' directives to mapps.directions. Both support city names
    consisting of multiple words. The order of the directives is not important.
    The current location will be used if no 'from' directive is present.
    
    Add 'in' directive to mapps.weather. If present the given city will be used
    instead of the current location. The city name may consist of multiple words.

--- Jarvis/mapps.py
+++ Jarvis/mapps.py
@@ -5,12 +5,13 @@ import requests
 from colorama import init
 from colorama import Fore, Back, Style
 
-def directions(data):
-    data = data.split(" ")
-    from_city = data[4]
-    to_city = data[6]
-    url = "https://www.google.co.in/maps/dir/"
-    url += "{0}/{1}".format(from_city, to_city)
+def directions(toCity, fromCity = 0):
+    if not fromCity:
+        send_url = 'http://freegeoip.net/json'
+        r = requests.get(send_url)
+        j = json.loads(r.text)
+        fromCity = j['city']
+    url = "https://www.google.co.in/maps/dir/{0}/{1}".format(fromCity, toCity)
     webbrowser.open(url)
 
 def locateme():
@@ -20,19 +21,20 @@ def locateme():
     hcity = j['city']
     print(Fore.BLUE + "You are at " + hcity + Fore.RESET)
 
-def weatherr():
-    send_url = 'http://freegeoip.net/json'
-    r = requests.get(send_url)
-    j = json.loads(r.text)
-    hcity = j['city']
+def weather(city = 0):
+    if not city:
+        send_url = 'http://freegeoip.net/json'
+        r = requests.get(send_url)
+        j = json.loads(r.text)
+        city = j['city']
     send_url = (
-        "http://api.openweathermap.org/data/2.5/weather?q={}"
-        "&APPID=ab6ec687d641ced80cc0c935f9dd8ac9&units=metric".format(hcity)
+        "http://api.openweathermap.org/data/2.5/weather?q={0}"
+        "&APPID=ab6ec687d641ced80cc0c935f9dd8ac9&units=metric".format(city)
     )
     r = requests.get(send_url)
     j = json.loads(r.text)
-    weath = j['main']['temp']
-    print(Fore.BLUE + "It's " + str(weath) + " °C" + Fore.RESET)
+    temperature = j['main']['temp']
+    print(Fore.BLUE + "It's " + str(temperature) + " °C in " + str(city) + Fore.RESET)
 
 
 def nearme(data):
