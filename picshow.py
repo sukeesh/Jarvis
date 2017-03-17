991e9d61f3f42200e773a38a6e676ad2a08d1b1e
StefanosChaliasos <t8140192@dias.aueb.gr>
2017-03-17 14:43:41 +0200

    remove old files

--- picshow.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-import webbrowser
-
-def showpics(strr):
-    strr = strr.split(" ")
-    query = strr[4]
-    url = "https://www.google.co.in/search?tbm=isch&q={}".format(query)
-    webbrowser.open(url)
