049c6f89b8774119af5bc283a67e4e1128dc5760
RottenCrab <ndoureliadis.aueb@gmail.com>
2017-03-19 00:50:52 +0200

    question solved

--- Jarvis/Jarvis.py
+++ Jarvis/Jarvis.py
@@ -1,8 +1,23 @@
 class Jarvis:
     def __init__(self):
-        self.actions = ["how are you",
-                        "what time is it",
-                        "weather",
-                        "near me"]
-        self.reactions = []
+        self.actions = {"how are you": "trash_talk",
+                        "weather": "weather",
+                        "open camera": "cheese",
 
+                        }
+
+    def actions(self, key):
+        def trash_talk():
+            pass
+        def weather():
+            pass
+        def cheese():
+            pass
+        locals()[key]()
+
+    def user_input(self):
+        pass
+
+    def executor(self):
+        pass
+    
