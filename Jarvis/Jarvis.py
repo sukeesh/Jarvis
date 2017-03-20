ebeed14f1064998be86b38558deec6b6d50662c2
RottenCrab <ndoureliadis.aueb@gmail.com>
2017-03-21 01:38:14 +0200

    add help section.

--- Jarvis/Jarvis.py
+++ Jarvis/Jarvis.py
@@ -149,6 +149,35 @@ class Jarvis:
             print(Fore.RED + "Goodbye, see you later!" + Fore.RESET)
             exit()
 
+        def help():
+            """
+            This method displays help about Jarvis.
+            :return: Nothing to return.
+            """
+            print Fore.BLUE + '-Usage: ' + Fore.RESET
+            print Fore.BLUE + ' Type any of the following commands to interact with Jarvis.' + Fore.RESET
+            print Fore.GREEN + '[*] Help: To see this message' + Fore.RESET
+            print Fore.GREEN + '[*] How are you?: To react with Jarvis!' + Fore.RESET
+            print Fore.GREEN + '[*] Open Camera: To open "cheese" program (camera).' + Fore.RESET
+            print Fore.GREEN + '[*] What time is it: To check the time.' + Fore.RESET
+            print Fore.GREEN + '[*] Where am i: To pinpoint your location.' + Fore.RESET
+            print Fore.GREEN + '[*] Near me: To see nearby locations.' + Fore.RESET
+            print Fore.GREEN + '[*] Music: To listen some good Music!' + Fore.RESET
+            print Fore.GREEN + '[*] Increase Volume: To increase your system volume.' + Fore.RESET
+            print Fore.GREEN + '[*] Decrease Volume: To decrease your system volume.' + Fore.RESET
+            print Fore.GREEN + '[*] Hotspot Start: To set up your own hotspot.' + Fore.RESET
+            print Fore.GREEN + '[*] Hotspot Stop: To stop your personal hotspot.' + Fore.RESET
+            print Fore.GREEN + '[*] Search for a string in a file: Match patterns in a string using regex.' + Fore.RESET
+            print Fore.GREEN + '[*] Check RAM: Detailed RAM usage.' + Fore.RESET
+            print Fore.GREEN + '[*] Todo: An ordinary TODO list.' + Fore.RESET
+            print Fore.GREEN + '[*] News: Get an update about the news!' + Fore.RESET
+            print Fore.GREEN + '[*] Show me pics of: Displays the selected pics.' + Fore.RESET
+            print Fore.GREEN + '[*] Evaluate: To get your calculations done!' + Fore.RESET
+            print Fore.GREEN + '[*] Show me directions from: Get directions about your destination!' + Fore.RESET
+            print Fore.GREEN + '[*] quit: Close the session with Jarvis...' + Fore.RESET
+            print Fore.GREEN + '[*] exit: Close the session with Jarvis...' + Fore.RESET
+            print Fore.GREEN + '[*] Goodbye: Close the session with Jarvis...' + Fore.RESET
+
         def error():
             """
             In case of an error or typo during user's input we notify the
