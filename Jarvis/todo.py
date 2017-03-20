c1955e3620d198670689d186f35e59082ffecfe0
Lucas Keune <lucas.keune@rwth-aachen.de>
2017-03-20 11:48:13 +0100

    Add due date and date parsing to todo list
    Add a due date to existing item on the todo list with
    'todo add due [date] <index> <date>'.
    
    Currently supported date and time formats:
    - 2017-03-25
    - 17-03-25
    - 25.03.2017
    - 25.03.17
    - 17:30
    - 5:30PM

--- Jarvis/todo.py
+++ Jarvis/todo.py
@@ -3,12 +3,23 @@ import os
 import sys
 import json
 
+from datetime import datetime as dt
+import re
+
 from colorama import init
 from colorama import Fore, Back, Style
 
+def json_serial(obj):
+    """JSON serializer for objects not serializable by default json code"""
+
+    if isinstance(obj, dt):
+        serial = obj.isoformat()
+        return serial
+    raise TypeError ("Type not serializable")
+
 def writeToFile():
     with open("todolist.txt", "w+") as f:
-        json.dump(todoList, f)
+        json.dump(todoList, f, default=json_serial)
 
 def _print(data):
     x = 0
@@ -18,6 +29,8 @@ def _print(data):
         if 'priority' in l and l['priority'] >= 100:
             sys.stdout.write(Fore.RED)
         print("<{2}> {0} [{1}%]".format(l['name'], l['complete'], x) + Fore.RESET)
+        if 'due' in l:
+            print("\tDue at {0}".format(l['due']))
         if 'comment' in l:
             print("\t{0}".format(l['comment']))
         x += 1
@@ -25,6 +38,43 @@ def _print(data):
 def sort(data):
     return sorted(data, key = lambda k: (-k['priority'] if 'priority' in k else 0, k['complete']))
     
+def parseDate(data):
+    elements = data.split()
+    parseString = ""
+    dateString = ""
+    hasDate = False
+    for d in elements:
+        if re.match("^[0-9]{2}-[0-1][0-9]-[0-3][0-9]", d):
+            parseString += "%y-%m-%d "
+            hasDate = True
+        elif re.match("^[1-9][0-9]{3}-[0-1][0-9]-[0-3][0-9]", d):
+            parseString += "%Y-%m-%d "
+            hasDate = True
+        elif re.match("^[0-3][0-9]-[0-1][0-9]-[0-9]{2}", d):
+            parseString += "%d.%m.%y "
+            hasDate = True
+        elif re.match("^[0-3][0-9]-[0-1][0-9]-[1-9][0-9]{3}", d):
+            parseString += "%d.%m.%Y "
+            hasDate = True
+
+        elif re.match("^[0-1][0-9]:[0-5][0-9][AP]M", d):
+            parseString += "%I:%M%p "
+        elif re.match("^[0-2][0-9]:[0-5][0-9]", d):
+            parseString += "%H:%M "
+
+        else:
+            print("Unknown Format: {0}".format(d))
+            continue
+        dateString += d + " "
+    if not hasDate:
+        dateString += dt.today().strftime("%Y-%m-%d")
+        parseString += "%Y-%m-%d"
+    try:
+        return dt.strptime(dateString, parseString)
+    except ValueError:
+        print(Fore.RED + "Date or time out of Range." + Fore.RESET)
+        return dt.today()
+
 def todoHandler(data):
     global todoList
     if "add" in data:
@@ -37,6 +87,15 @@ def todoHandler(data):
                 print("No such todo")
                 return
             todoList['items'][index]['comment'] = " ".join(words[1:])
+        elif "due" in data or "due date" in data:
+            data = data.replace("due date", "", 1)
+            data = data.replace("due", "", 1)
+            words = data.split()
+            index = int(words[0])
+            if(index<0 or index>=len(todoList['items'])):
+                print("No such todo")
+                return
+            todoList['items'][index]['due'] = parseDate(" ".join(words[1:]))
         else:
             data = " ".join(data.split())
             newItem = {'complete':0}
