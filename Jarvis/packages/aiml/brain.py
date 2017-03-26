97cc8f6d91c4e32217f96370317a61369e4c1789
Brent <hand.brent@gmail.com>
2017-03-26 01:28:07 -0400

    optimized loading of aiml files

--- Jarvis/packages/aiml/brain.py
+++ Jarvis/packages/aiml/brain.py
@@ -2,6 +2,7 @@ import aiml
 
 import os
 
+#this sets the path to the modules directory not the directory it was call from
 module_path = os.path.dirname(__file__)
 
 
@@ -9,17 +10,16 @@ class Brain:
 
     def __init__(self):
         self.kernel = aiml.Kernel()
-        self.kernel.verbose(False) # to remove all of the system loading output
-        self.kernel.learn(os.path.join(module_path, 'chat.aiml'))
+        #self.kernel.verbose(False) # rmove system output
+
+        # brain file already exists load it
+        if os.path.isfile(os.path.join(module_path,"bot_brain.brn")):
+            self.kernel.bootstrap(brainFile = os.path.join(module_path, "bot_brain.brn"))
+        # if brain file doesnt exist load std-startup.xml and create and save brain file
+        else:
+            self.kernel.bootstrap(learnFiles = os.path.join(module_path, "std-startup.xml"), commands = "load aiml b")
+            self.kernel.saveBrain(os.path.join(module_path, "bot_brain.brn"))
 
 
     def respond(self, text):
         return self.kernel.respond(text)
-
-
-# Create the kernel and learn AIML files
-
-
-# Press CTRL-C to break this loop
-#while True:
-#    print kernel.respond(raw_input("Enter your message >> "))
