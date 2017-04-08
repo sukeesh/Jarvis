9b78e28ab0d216089d92afbea7617384716c0800
Brent <hand.brent@gmail.com>
2017-04-08 02:06:58 -0400

    fixed test and added two functinos for handling createing and deleting brain files

--- Jarvis/packages/aiml/brain.py
+++ Jarvis/packages/aiml/brain.py
@@ -18,13 +18,21 @@ class Brain:
             self.kernel.bootstrap(brainFile = os.path.join(module_path, "bot_brain.brn"))
         # if brain file doesnt exist load std-startup.xml and create and save brain file
         else:
-            self.kernel.learn(os.path.join(module_path, "chat.aiml"))
-            self.kernel.learn(os.path.join(module_path, "emotion.aiml"))
-
-            #This file should go last. It contains wild cards
-            self.kernel.learn(os.path.join(module_path, "default.aiml"))
-            self.kernel.saveBrain(os.path.join(module_path, "bot_brain.brn"))
+            self.create_brain()
 
 
     def respond(self, text):
         return self.kernel.respond(text)
+
+
+    def create_brain(self):
+        self.kernel.learn(os.path.join(module_path, "chat.aiml"))
+        self.kernel.learn(os.path.join(module_path, "emotion.aiml"))
+
+        #This file should go last. It contains wild cards
+        self.kernel.learn(os.path.join(module_path, "default.aiml"))
+        self.kernel.saveBrain(os.path.join(module_path, "bot_brain.brn"))
+
+
+    def remove_brain():
+        os.remove(os.path.join(module_path,"bot_brain.brn"))
