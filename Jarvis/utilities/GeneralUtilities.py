6a3e26a21b61831fe37e5dd4fcd45bedcf26ad49
StefanosChaliasos <t8140192@dias.aueb.gr>
2017-04-11 22:15:20 +0300

    add comment

--- Jarvis/utilities/GeneralUtilities.py
+++ Jarvis/utilities/GeneralUtilities.py
@@ -5,6 +5,13 @@ def wordIndex(data, word):
     return wordList.index(word)
 
 def print_say(text, self, color=""):
+	"""
+        This method give the jarvis the ability to print a text
+	and talk when sound is enable.
+        :param text: the text to print (or talk)
+               color: Fore.COLOR (ex Fore.BLUE), color for text
+        :return: Nothing to return.
+        """
 	if self.enable_voice:
 		self.speech.text_to_speech(text)
 	print(color + text + Fore.RESET)
