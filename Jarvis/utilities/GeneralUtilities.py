b9c54595762d3f2a48544bbfb48775e04b5b98f6
Stefanos <t8140192@dias.aueb.gr>
2017-04-11 05:31:10 +0300

    create print_say method

--- Jarvis/utilities/GeneralUtilities.py
+++ Jarvis/utilities/GeneralUtilities.py
@@ -1,3 +1,10 @@
+from colorama import Fore
+
 def wordIndex(data, word):
     wordList = data.split()
     return wordList.index(word)
+
+def print_say(text, self, color=""):
+	if self.enable_voice:
+		self.speech.text_to_speech(text)
+	print(color + text + Fore.RESET)
