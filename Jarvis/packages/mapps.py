ba9483acbdd3a7915f05b9a8108158f6a700d309
Luis Silva <luiseduardofigueiredosilva@gmail.com>
2017-05-12 21:25:53 +0100

    Weather now shows in Fahrenheit if current location is in US (#73)

--- Jarvis/packages/mapps.py
+++ Jarvis/packages/mapps.py
@@ -27,15 +27,30 @@ def locateme():
 def weather(city = 0):
     if not city:
         city = getLocation()['city']
-    send_url = (
-        "http://api.openweathermap.org/data/2.5/weather?q={0}"
-        "&APPID=ab6ec687d641ced80cc0c935f9dd8ac9&units=metric".format(city)
-    )
+
+    # Checks country
+    country = getLocation()['country_name']
+
+    # If country is US, shows weather in Fahrenheit
+    if country == 'United States':
+        send_url = (
+            "http://api.openweathermap.org/data/2.5/weather?q={0}"
+            "&APPID=ab6ec687d641ced80cc0c935f9dd8ac9&units=imperial".format(city)
+        )
+        unit = ' ºF in '
+
+    # If country is not US, shows weather in Celsius
+    else:
+        send_url = (
+            "http://api.openweathermap.org/data/2.5/weather?q={0}"
+            "&APPID=ab6ec687d641ced80cc0c935f9dd8ac9&units=metric".format(city)
+        )
+        unit = ' ºC in '
     r = requests.get(send_url)
     j = json.loads(r.text)
     temperature = j['main']['temp']
     description = j['weather'][0]['main']
-    print(Fore.BLUE + "It's " + str(temperature) + " °C in " + str(city) + " (" + str(description) + ")" + Fore.RESET)
+    print(Fore.BLUE + "It's " + str(temperature) + unit + str(city) + " (" + str(description) + ")" + Fore.RESET)
 
 def searchNear(things, city = 0):
     if city:
