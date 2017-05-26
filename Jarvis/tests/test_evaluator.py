690071208b60b45658d72a9bc36dc4ae1cfbe5b9
Ryan Arredondo <sucharyan@gmail.com>
2017-05-26 17:35:12 -0600

    Added unittest for calc method in Jarvis/packages/evaluator.py

---
+++ Jarvis/tests/test_evaluator.py
@@ -0,0 +1,38 @@
+import unittest
+import cmath
+import sys
+from StringIO import StringIO
+
+from colorama import Fore
+
+from Jarvis import Jarvis
+from packages.evaluator import calc
+
+class EvaluatorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.jarvis = Jarvis()
+        sys.stdout = StringIO()
+
+    def test_calc(self):
+        # Test a basic calculation
+        calc("2 powER 7 PLUS (6.7 Minus 3) diVided BY 0.45 bY 3", self.jarvis)
+        sys.stdout.seek(0)
+        output = sys.stdout.read().strip()
+        result = Fore.BLUE + str(2 ** 7 + (6.7 - 3) / 0.45 / 3) + Fore.RESET
+        self.assertEqual(output, result)
+
+        # And now for something a little more _complex_
+        sys.stdout = StringIO()
+        x = calc("(1 pluS 9.1j)^3.14129 mINUS 2.712", self.jarvis)
+        sys.stdout.seek(0)
+        output = sys.stdout.read().strip()
+        result = Fore.BLUE + str((1+9.1j)**3.14129 - 2.712) + Fore.RESET
+        self.assertEqual(output, result)
+
+    def tearDown(self):
+        sys.stdout = sys.__stdout__
+
+
+if __name__ == '__main__':
+    unittest.main()
