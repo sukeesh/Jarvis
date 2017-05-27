00edec68ef5e37f2d3c15c5f22bee323a98b8761
Shehriyar Qureshi <SShehriyar266@gmail.com>
2017-05-27 15:21:27 +0500

    Modified to Pep8 and removed unused modules

--- Jarvis/packages/mapps.py
+++ Jarvis/packages/mapps.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
-import json, webbrowser
+import json
+import webbrowser
 import requests
-from colorama import Fore, Back, Style
+from colorama import Fore
 
 location = 0
+
 
 def getLocation():
     global location
@@ -14,17 +16,20 @@ def getLocation():
         location = json.loads(r.text)
     return location
 
-def directions(toCity, fromCity = 0):
+
+def directions(toCity, fromCity=0):
     if not fromCity:
         fromCity = getLocation()['city']
     url = "https://www.google.com/maps/dir/{0}/{1}".format(fromCity, toCity)
     webbrowser.open(url)
 
+
 def locateme():
     hcity = getLocation()['city']
     print(Fore.BLUE + "You are at " + hcity + Fore.RESET)
 
-def weather(city = 0):
+
+def weather(city=0):
     if not city:
         city = getLocation()['city']
 
@@ -35,7 +40,8 @@ def weather(city = 0):
     if country == 'United States':
         send_url = (
             "http://api.openweathermap.org/data/2.5/weather?q={0}"
-            "&APPID=ab6ec687d641ced80cc0c935f9dd8ac9&units=imperial".format(city)
+            "&APPID=ab6ec687d641ced80cc0c935f9dd8ac9&units=" +
+            "imperial".format(city)
         )
         unit = ' ºF in '
 
@@ -50,13 +56,18 @@ def weather(city = 0):
     j = json.loads(r.text)
     temperature = j['main']['temp']
     description = j['weather'][0]['main']
-    print(Fore.BLUE + "It's " + str(temperature) + unit + str(city) + " (" + str(description) + ")" + Fore.RESET)
+    print(Fore.BLUE + "It's " + str(temperature) + unit +
+          str(city) + " (" + str(description) + ")" + Fore.RESET)
 
-def searchNear(things, city = 0):
+
+def searchNear(things, city=0):
     if city:
-        print(Fore.GREEN + "Hold on!, I'll show " + things + " near " + city + Fore.RESET)        
+        print(Fore.GREEN + "Hold on!, I'll show " + things + " near " + city
+              + Fore.RESET)
         url = "https://www.google.com/maps/search/{0}+{1}".format(things, city)
     else:
-        print(Fore.GREEN + "Hold on!, I'll show " + things + " near you" + Fore.RESET)
-        url = "https://www.google.com/maps/search/{0}/@{1},{2}".format(things, getLocation()['latitude'], getLocation()['longitude'])
+        print(Fore.GREEN + "Hold on!, I'll show " + things + " near you"
+              + Fore.RESET)
+        url = "https://www.google.com/maps/search/{0}/@{1},{2}".format(
+            things, getLocation()['latitude'], getLocation()['longitude'])
     webbrowser.open(url)
