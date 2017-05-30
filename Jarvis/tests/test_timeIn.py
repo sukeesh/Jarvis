10202d36eb60d77f80d3b3bfb5508037a6d8e40e
Timoteo <phillipstr@gmail.com>
2017-05-30 13:46:34 -0400

    Adjusts test_timIn to run in UTC

--- Jarvis/tests/test_timeIn.py
+++ Jarvis/tests/test_timeIn.py
@@ -12,13 +12,21 @@ class TimeInTest(unittest.TestCase):
     def test_timeIn(self):
         """ test the timeIn.main function against time.ctime """
 
-        timeIn.main(self, "time in New York City, NY")
+         # call timeIn.main and redirect stdout to a StringIO object
+        timeIn.main(self, "time in Greenwich, UK")
         sys.stdout.seek(0)
         output = sys.stdout.read()
-        result = output.split(' ')[12][0:8]
+        result = output.split(' ')[10][0:8]
 
+        # What is ctime in you current local?
         time_here = time.ctime(time.time()).split(' ')[3]
 
+        # Adjust 'result' variable for your current local's timezone
+        adjustment = time.timezone / 3600
+        result_list = result.split(':')
+        hour = (int(result_list[0]) - adjustment) % 24
+        result = ':'.join([str(hour), result_list[1], result_list[2]])   
+            
         self.assertEqual(result, time_here)
 
     def tearDown(self):
