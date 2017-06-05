3eb6e34835fc25e0837087fdc83d492af7c0ef8d
Ana Isabel <imacswv@gmail.com>
2017-06-05 13:08:49 +0200

    Solved problem with selecting the song name. Added corresponding tests

---
+++ Jarvis/tests/test_music.py
@@ -0,0 +1,25 @@
+import unittest
+import os
+from mock import call, patch
+from packages import music
+
+class MusicTest(unittest.TestCase):
+
+    def setUp(self):
+        self.song_name = 'torero chayanne'
+
+    def test_song_is_searched_with_the_given_song_name(self):
+        first_popen_call = call("ls | grep -i " +'"'+ self.song_name +'"')
+        first_system_call = call("instantmusic -s " + self.song_name)
+        with patch.object(os, 'system', return_value=None) as mock_system:
+            with patch.object(os, 'popen', return_value=os.popen("")) as mock_popen:
+                music.play(self.song_name)
+                mock_popen.assert_has_calls([first_popen_call])
+                mock_system.assert_has_calls([first_system_call])
+
+    def test_song_is_not_searched_without_a_song_name(self):
+        with patch.object(os, 'system', return_value=None) as mock_system:
+            with patch.object(os, 'popen', return_value=os.popen("")) as mock_popen:
+                music.play('')
+                self.assertFalse(mock_popen.called)
+                self.assertFalse(mock_system.called)
