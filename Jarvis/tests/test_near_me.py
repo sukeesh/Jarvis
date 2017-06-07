aa4df6ab66471ecf46bf6ed99eedd5313a5e0476
Ana Isabel <imacswv@gmail.com>
2017-06-07 20:41:41 +0200

    Tests added for passing all the words with the near command to near_me.py their correct use in near_me.py

---
+++ Jarvis/tests/test_near_me.py
@@ -0,0 +1,16 @@
+import unittest
+from mock import patch
+from packages import near_me, mapps
+
+
+class NearMeTest(unittest.TestCase):
+
+    def setUp(self):
+        self.things = 'charities'
+        self.city = 'Valencia'
+
+    @patch.object(mapps, 'searchNear')
+    def test_what_to_search_where_is_passed_to_mapps(self, mock_search_near):
+        data = "{} | {}".format(self.things, self.city)
+        near_me.main(data)
+        mock_search_near.assert_called_once_with(self.things, self.city)
