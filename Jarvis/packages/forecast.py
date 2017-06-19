f4c8bfc60556166b0fa343d0b53891ef7c234f9f
Ana Isabel <imacswv@gmail.com>
2017-06-19 17:20:09 +0200

    location fetched from the command given. Tests included

--- Jarvis/packages/forecast.py
+++ Jarvis/packages/forecast.py
@@ -1,2 +1,16 @@
+from utilities.GeneralUtilities import print_say
+from colorama import Fore
+
 def main(self, s):
-    pass
+    cmd_key_words = ['check', 'weather', 'forecast', 'in', 'for']
+    cmd_words = s.strip().split()
+    # location will be defined by the words given that are not the key words
+    location = ' '.join(filter(lambda word: word.lower() not in cmd_key_words, cmd_words)).strip()
+
+    # if no location is given, use the current location
+    if not location:
+        print_say("Weather forecast in the current location for the next 7 days.", self, Fore.BLUE)
+        return
+
+    # otherwise use the location given
+    print_say("Weather forecast in {} for the next 7 days.".format(location), self, Fore.BLUE)
--- Jarvis/tests/test_forecast.py
+++ Jarvis/tests/test_forecast.py
@@ -2,11 +2,33 @@ import unittest
 from mock import patch
 from packages import forecast
 from CmdInterpreter import CmdInterpreter
+from colorama import Fore
 
 
 class ForecastTest(unittest.TestCase):
 
+    def setUp(self):
+        self.CI_instance = CmdInterpreter('', '')
+
     @patch.object(forecast, 'main')
     def test_forecast_called_from_do_check(self, forecast_mock):
-        CmdInterpreter('', '').do_check('check forecast')
+        self.CI_instance.do_check('check forecast')
         forecast_mock.assert_called()
+
+    @patch.object(forecast, 'print_say')
+    def test_print_as_expected_when_no_location(self, print_say_mock):
+        forecast.main(self.CI_instance, 'check forecast')
+        print_say_mock.assert_called_with(
+            "Weather forecast in the current location for the next 7 days.",
+            self.CI_instance,
+            Fore.BLUE
+        )
+
+    @patch.object(forecast, 'print_say')
+    def test_print_as_expected_with_location(self, print_say_mock):
+        forecast.main(self.CI_instance, 'check forecast in New York')
+        print_say_mock.assert_called_with(
+            "Weather forecast in New York for the next 7 days.",
+            self.CI_instance,
+            Fore.BLUE
+        )
