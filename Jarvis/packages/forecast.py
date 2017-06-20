f2727af1b1d1398477f9f6aad0cc30fac20e3008
Ana Isabel <imacswv@gmail.com>
2017-06-20 15:52:07 +0200

    Forecast information formatted. Default current location added. Tests missing.

--- Jarvis/packages/forecast.py
+++ Jarvis/packages/forecast.py
@@ -4,6 +4,7 @@ from colorama import Fore
 import json
 import mapps
 import requests
+from utilities.dateTime import WeekDay
 
 def main(self, s):
     cmd_key_words = ['check', 'weather', 'forecast', 'in', 'for']
@@ -11,28 +12,25 @@ def main(self, s):
     # location will be defined by the words given that are not the key words
     location = ' '.join(filter(lambda word: word.lower() not in cmd_key_words, cmd_words)).strip()
 
+    current_location = mapps.getLocation()
+
     # if no location is given, use the current location
     if not location:
-        print_say("Weather forecast in the current location for the next 7 days.", self, Fore.BLUE)
-        # if no 'location' provided, store the current location in 'location'
-        location = 'Madrid'
-
-    # otherwise use the location given
-    print_say("Weather forecast in {} for the next 7 days.".format(location.title()), self, Fore.BLUE)
+        location = "{},{}".format(current_location['city'], current_location['country_code'])
  
 
-    country = mapps.getLocation()['country_name']
+    country = current_location['country_name']
 
     # If country is not US, shows weather in Celsius
     units = {
         'url_units': 'metric',
-        'str_units': ' ºC in '
+        'str_units': 'ºC'
     }
     # If country is US, shows weather in Fahrenheit
     if country == 'United States':
         units = {
             'url_units': 'imperial',
-            'str_units': ' ºF in '
+            'str_units': 'ºF'
         }
 
     send_url = (
@@ -42,3 +40,29 @@ def main(self, s):
 
     r = requests.get(send_url)
     j = json.loads(r.text)
+
+    week_from_today = WeekDay().get_week_from_today()
+
+    try:
+        print_say(
+            "Weather forecast in {} for the next 7 days.".format(
+                j['city']['name'].title()
+            ),
+            self,
+            Fore.BLUE
+        )
+        for cnt, day_dict in enumerate(j['list']):
+            print_say("{}:".format(week_from_today[cnt]), self, Fore.BLUE)
+            print_say("\tWeather: {}".format(day_dict['weather'][0]['main']), self, Fore.BLUE)
+            print_say(
+                "\tMax temperature: {} {}".format(round(day_dict['temp']['max'], 1), units['str_units']),
+                self,
+                Fore.BLUE
+            )
+            print_say(
+                "\tMin temperature: {} {}\n".format(round(day_dict['temp']['min'], 1), units['str_units']),
+                self,
+                Fore.BLUE
+            )
+    except KeyError:
+        print_say("The forecast information could not be found.", self, Fore.RED)
