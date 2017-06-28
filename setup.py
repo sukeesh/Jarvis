bdc82594de869ef617f279719cf2896eed802208
appi <arpitkumar147@gmail.com>
2017-06-28 12:09:53 +0530

    prepare for pip release

---
+++ setup.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='Jarvis',
+    version='',
+    description='Personal Assistant for Linux',
+    url='https://github.com/sukeesh/Jarvis',
+    author='Sukeesh',
+    author_email='vsukeeshbabu@gmail.com',
+    keywords=['linux','personal-assistant'],
+    license='MIT',
+    packages=['Jarvis'],
+    classifiers=['Programming Language :: Python :: 2.7','License :: MIT License'],
+    install_requires=required
+)
