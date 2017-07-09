8113ef9ae91c1eea8ec8a39788d970c11add34ec
Roberto Oliveira <robertoguimaraes8@gmail.com>
2017-07-08 23:22:12 -0300

    fix crashing in 'near' command when using without parameter
    When typing 'near' command without any parameter, a ValueError is raised
    and jarvis crashes.
    
    This patch shows the 'near' help message instead of crashing.

--- jarviscli/packages/near_me.py
+++ jarviscli/packages/near_me.py
@@ -1,10 +1,17 @@
 from utilities.GeneralUtilities import wordIndex
+import CmdInterpreter
 import mapps
 
 
 def main(data):
     word_list = data.split()
-    things = " ".join(word_list[0:wordIndex(data, "|")])
+    try:
+        things = " ".join(word_list[0:wordIndex(data, "|")])
+    except ValueError:
+        cmd = CmdInterpreter.CmdInterpreter("", "")
+        cmd.help_near()
+        return
+
     if " me" in data:
         city = 0
     else:
