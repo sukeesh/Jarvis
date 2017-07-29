1931c6df46289288e5c5b98992b2a5b269e0bd86
appi <arpitkumar147@gmail.com>
2017-07-29 10:27:55 +0530

    PEP8

--- jarviscli/CmdInterpreter.py
+++ jarviscli/CmdInterpreter.py
@@ -82,7 +82,6 @@ class CmdInterpreter(Cmd):
                         "say",
                         {"screen": ("off",)},
                         "shutdown",
-                        
                         "todo",
                         {"tell": ("joke",)},
                         "umbrella",
