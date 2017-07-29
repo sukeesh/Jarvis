de6030193532f02a5da14f4fe42a7e65321aa97b
appi <arpitkumar147@gmail.com>
2017-07-29 10:09:37 +0530

    PEP8

--- jarviscli/packages/picshow.py
+++ jarviscli/packages/picshow.py
@@ -3,5 +3,6 @@ import webbrowser
 
 
 def showpics(string):
-    url = "https://www.google.com/search?tbm=isch&q={}".format(string.replace("of",""))
+    url = "https://www.google.com/search?tbm=isch&q={}".format(
+        string.replace("of", ""))
     webbrowser.open(url)
