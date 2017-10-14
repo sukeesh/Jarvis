13afa1b02f37ebd3ddbb4cfaa15410c69d954eb4
appi <arpitkumar147@gmail.com>
2017-10-14 16:43:18 +0530

    Add virtualenv for Ubuntu

--- setup.sh
+++ setup.sh
@@ -4,13 +4,13 @@ jarvispath=$PWD
 touch jarvis
 chmod +rwx jarvis
 exec 3<> jarvis
-
-    # Let's print some text to fd 3
+    
     echo "#!/bin/bash" >&3
+    echo "source $jarvispath/env/bin/activate" >&3
     echo "python $jarvispath/jarviscli/" >&3
 
-# Close fd 3
 exec 3>&-
+
 sudo mv jarvis /usr/local/bin/jarvis
 
 UNAME=$(uname -s)
@@ -38,8 +38,12 @@ if [[ "$OS" == "Fedora" ]]; then
 elif [[ "$OS" == "Ubuntu" ]] || [[ "$OS" == "LinuxMint" ]]; then
   sudo apt-get install ffmpeg python-imdbpy python-notify2
   sudo apt-get install python-dbus libssl-dev libffi-dev
-  sudo -H pip install -r requirements.txt
   sudo apt-get install chromium-chromedriver
+  sudo pip install virtualenv
+  virtualenv env
+  virtualenv -p /usr/bin/python2.7 env
+  source env/bin/activate
+  pip install -r requirements.txt
 elif [[ "$OS" == "Kali" ]]; then
   apt-get install ffmpeg
   apt-get install python-notify2
