b6c2968f7ea43629b8481ea14e4dfe6fdc56aa08
GrantFleming <grantgfleming@googlemail.com>
2017-12-14 19:50:00 +0000

    Added conditional to hackathon.py so that if it BeautifulSoup has trouble populating upcoming it prints a message to the user instead of crashing out the program.

--- jarviscli/packages/hackathon.py
+++ jarviscli/packages/hackathon.py
@@ -9,11 +9,16 @@ def find_hackathon(self):
     res = driver.page_source
     soup = BeautifulSoup(res, 'lxml')
     upcoming = soup.find('div', {'class': 'upcoming challenge-list'})
-    all_hackathons = upcoming.find_all('div', {'class': 'challenge-content'})
 
-    for i, hackathon in enumerate(all_hackathons, 1):
-        challenge_type = hackathon.find('div', {'class': 'challenge-type'}).text.replace("\n", " ").strip()
-        challenge_name = hackathon.find('div', {'class': 'challenge-name'}).text.replace("\n", " ").strip()
-        date_time = hackathon.find('div', {'class': 'challenge-list-meta challenge-card-wrapper'}).text.replace("\n",
+    if upcoming is not None:
+
+        all_hackathons = upcoming.find_all('div', {'class': 'challenge-content'})
+
+        for i, hackathon in enumerate(all_hackathons, 1):
+            challenge_type = hackathon.find('div', {'class': 'challenge-type'}).text.replace("\n", " ").strip()
+            challenge_name = hackathon.find('div', {'class': 'challenge-name'}).text.replace("\n", " ").strip()
+            date_time = hackathon.find('div', {'class': 'challenge-list-meta challenge-card-wrapper'}).text.replace("\n",
                                                                                                                 " ").strip()
-        print("[{}] {}\n{}\n{}\n\n".format(str(i), challenge_name, challenge_type, date_time))
+            print("[{}] {}\n{}\n{}\n\n".format(str(i), challenge_name, challenge_type, date_time))
+    else:
+        print "No hackathon data found."
