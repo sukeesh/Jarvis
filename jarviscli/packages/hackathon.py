0c177fba221a666dcce19df3159ae59a6faf1977
appi147 <arpitkumar147@gmail.com>
2017-12-18 16:28:50 +0530

    PEP8 fix

--- jarviscli/packages/hackathon.py
+++ jarviscli/packages/hackathon.py
@@ -17,8 +17,7 @@ def find_hackathon(self):
         for i, hackathon in enumerate(all_hackathons, 1):
             challenge_type = hackathon.find('div', {'class': 'challenge-type'}).text.replace("\n", " ").strip()
             challenge_name = hackathon.find('div', {'class': 'challenge-name'}).text.replace("\n", " ").strip()
-            date_time = hackathon.find('div', {'class': 'challenge-list-meta challenge-card-wrapper'}).text.replace("\n",
-                                       " ").strip()
+            date_time = hackathon.find('div', {'class': 'challenge-list-meta challenge-card-wrapper'}).text.replace("\n", " ").strip()
             print("[{}] {}\n{}\n{}\n\n".format(str(i), challenge_name, challenge_type, date_time))
     else:
         print "No hackathon data found."
