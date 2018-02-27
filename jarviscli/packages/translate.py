f9d8ed6b97e06512c5bbb925150a35909d2d85b1
Albin Remnestål <albin.remnestal@gmail.com>
2018-02-27 16:40:55 +0100

    refactor: decrease complexity of translate.py
    Decreases the cyclic complexity number from 16 to 7 by removing dead code.
    
    Closes #17

--- jarviscli/packages/translate.py
+++ jarviscli/packages/translate.py
@@ -3,57 +3,51 @@ from utilities.GeneralUtilities import print_say
 from googletrans.constants import LANGCODES, LANGUAGES, SPECIAL_CASES
 import six
 
-
 def main(self):
+    """ translates a sentance """
 
-    '''
-        source language
-    '''
+    def get_input():
+        """ reads input from stdin
+            implementation differ on whether python2 or python3 is used
+        """
+        if six.PY2:
+            return raw_input()
+        else:
+            return input()
 
+    # make the user provide a source language
     print_say('\nEnter source language ', self)
-    if six.PY2:
-        srcs = raw_input()
-    else:
-        srcs = input()
+    srcs = get_input()
     while (srcs not in LANGUAGES) and (srcs not in SPECIAL_CASES) and (srcs not in LANGCODES):
-        if srcs in SPECIAL_CASES:
-            srcs = SPECIAL_CASES[srcs]
-        elif srcs in LANGCODES:
-            srcs = LANGCODES[srcs]
-        else:
-            print_say("\nInvalid source language\nEnter again", self)
-            if six.PY2:
-                srcs = raw_input()
-            else:
-                srcs = input()
+        print_say("\nInvalid source language\nEnter again", self)
+        srcs = get_input()
+
+    # make the user provide a 'destination' language
     print_say('\nEnter destination language ', self)
-    if six.PY2:
-        des = raw_input()
-    else:
-        des = input()
+    des = get_input()
     while (des not in LANGUAGES) and (des not in SPECIAL_CASES) and (des not in LANGCODES):
-        if des in SPECIAL_CASES:
-            des = SPECIAL_CASES[des]
-        elif des in LANGCODES:
-            des = LANGCODES[des]
-        else:
-            print_say("\nInvalid destination language\nEnter again", self)
-            if six.PY2:
-                des = raw_input()
-            else:
-                des = input()
+        print_say("\nInvalid destination language\nEnter again", self)
+        des = get_input()
+
+    # make the user provide a sentence to translate
     print_say('\nEnter text ', self)
-    if six.PY2:
-        tex = raw_input()
-    else:
-        tex = input()
+    text = get_input()
     translator = Translator()
-    result = translator.translate(tex, dest=des, src=srcs)
-    result = u"""
-[{src}] {original}
-    ->
-[{dest}] {text}
-[pron.] {pronunciation}
-    """.strip().format(src=result.src, dest=result.dest, original=result.origin,
-                       text=result.text, pronunciation=result.pronunciation)
+    result = translator.translate(text, dest=des, src=srcs)
+
+    output_lines = [
+        u'[{src}] {original}',
+        u'    ->',
+        u'[{dest}] {text}',
+        u'[pron.] {pronunciation}'
+    ]
+
+    result = '\n'.join(output_lines).strip().format(
+        src=result.src,
+        dest=result.dest,
+        original=result.origin,
+        text=result.text,
+        pronunciation=result.pronunciation
+    )
+
     print(result)
