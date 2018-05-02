084341a757ca787ea4eecf075bb8b8fdb3c8dd19
kfragkedaki <fr-claire@hotmail.com>
2018-05-02 13:42:31 +0300

    Improve currencyconv with lookup table and help option

--- jarviscli/packages/currencyconv.py
+++ jarviscli/packages/currencyconv.py
@@ -1,12 +1,18 @@
 from forex_python.converter import CurrencyRates
 from forex_python.bitcoin import BtcConverter
 from decimal import Decimal
+from six.moves import input
 import csv
 import os
 
 FILE_PATH = os.path.abspath(os.path.dirname(__file__))
 
-# currencyconv converts the given ammount to another currency using fore-python
+
+'''''
+ currencyconv converts the given amount to another currency using fore-python
+'''''
+
+
 def currencyconv(self, amount, fr, to):
 
     b = BtcConverter(force_decimal=True)
@@ -20,29 +26,41 @@ def currencyconv(self, amount, fr, to):
         result = c.convert(fr, to, Decimal(amount))
 
     print result
-    return result
 
-# find_currency includes if the input the user gave is appropriate
-# forex-python can only receive inputs that are in this format
+
+'''''
+find_currency creates a dict with the inputs that
+forex-python accepts
+'''''
+
 
 def find_currencies():
-    with open(os.path.join(FILE_PATH, "../../data/currencies.csv"), mode='r') as infile:
+    with open(os.path.join(FILE_PATH, "../data/currencies.csv"), mode='r') \
+    as infile:
         reader = csv.reader(infile)
-        mydict = {r: row[2] for row in reader for r in row}
+        mydict = {r.upper(): row[2] for row in reader for r in row[0:3]}
     return mydict
+
+
+'''''
+get_currency checks if the input the user gave is valid based on the
+dictionary of find_currencies
+'''''
+
 
 def get_currency(prompt, currencies):
 
-    first_time = 1
     while True:
-        c = raw_input(prompt).upper()
+        c = input(prompt).upper()
         if c in currencies:
             return currencies[c]
-        elif first_time == 1:
-            print("Please enter a valid country or currency!")
-            get_help = input("Type 1 to see valid inputs or 2 to continue: ")
-            if get_help == 1:
-                print(currencies.keys())
-            elif get_help == 2:
-                continue
-        first_time = 0
+        elif c == "show help".upper():
+            print(', '.join(set(currencies.values())))
+            prompt = 'Please enter a valid country or currency: '
+            continue
+        elif c == "try again".upper():
+            prompt = 'Please enter a valid country or currency: '
+            continue
+        else:
+            prompt = 'Type -show help- to see valid currencies ' + \
+            'or -try again- to continue: '
