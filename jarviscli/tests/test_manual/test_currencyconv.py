9ae0dd68250a46614ebf724e0305f4619e1905c8
kfragkedaki <fr-claire@hotmail.com>
2018-05-11 11:52:47 +0300

    Correcting test_currencyconv based on Codacy.com

--- jarviscli/tests/test_manual/test_currencyconv.py
+++ jarviscli/tests/test_manual/test_currencyconv.py
@@ -1,10 +1,8 @@
 import unittest
 from Jarvis import Jarvis
-from packages.currencyconv import currencyconv
-from packages.currencyconv import find_currencies
 
 
-def get_float(prompt, input):
+def get_float_input(prompt, input):
     while True:
         try:
             value = float(input.replace(',', '.'))
@@ -15,7 +13,7 @@ def get_float(prompt, input):
             continue
 
 
-def get_currency(prompt, currencies, input):
+def get_currency_input(prompt, currencies, input):
     """
     get_currency checks if the input the user gave is valid based on the
     dictionary of find_currencies
@@ -48,18 +46,18 @@ class CurrencyConvTest(unittest.TestCase):
 
     def test_get_currency(self):
         # normal case
-        self.assertEqual(get_currency('Enter a currency',
+        self.assertEqual(get_currency_input('Enter a currency',
                                       {'GREECE': 'EUR', 'EURO': 'EUR',
                                        'BITCOINS': 'BTC'}, 'Greece'), 'EUR')
 
     def test_get_float_int(self):
         # normal case
         print("Testing get_float_int checked")
-        self.assertEqual(get_float("", '1'), 1)
+        self.assertEqual(get_float_input("", '1'), 1)
 
     def test_get_float_comma(self):
         # change comma with dot
-        self.assertEqual(get_float('Enter a number: ', '12,6'), 12.6)
+        self.assertEqual(get_float_input('Enter a number: ', '12,6'), 12.6)
 
 
 if __name__ == '__main__':
