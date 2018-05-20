19794cc86e478715c56550c8f851438689b61138
Adelin <theskittlespenguin@gmail.com>
2018-05-20 03:33:14 +0300

    Added new feature: imgur
    Feature that uploads an image to imgur

---
+++ jarviscli/packages/imgur.py
@@ -0,0 +1,34 @@
+from utilities.GeneralUtilities import print_say
+import requests
+import os
+import json
+import base64
+
+
+def imgur(self, s):
+    s = os.path.abspath(s)
+    if os.path.isfile(s):
+        try:
+            url = "https://api.imgur.com/3/image"
+            headers = {"Authorization": "Client-ID 145b6ea95cf11b4"}
+
+            resp = requests.post(
+                url,
+                headers=headers,
+                data={
+                    'image': base64.b64encode(open(s, 'rb').read()),
+                    'type': 'base64'
+                }
+            )
+
+            objresp = json.loads(resp.text)
+
+            if objresp.get('success', False):
+                print_say('Here is your image: ' +
+                          objresp['data']['link'], self)
+            else:
+                print_say('Error: ' + objresp['data']['error'], self)
+        except Exception as e:
+            print_say("Error {0}".format(str(e.args[0])).encode("utf-8"), self)
+    else:
+        print_say("No such file", self)
