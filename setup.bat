5a7870aef2fefc5103816dfd33ea35569f4b1a29
Kristiyan Dimitrov <kdimitrov.business.com>
2018-10-20 17:48:41 +0200

    setup.bat no longer requires elevated priviliges

--- setup.bat
+++ setup.bat
@@ -1,24 +1,9 @@
 @echo off
 
-:CheckError
-if %errorlevel% neq 0 exit /b %errorlevel%
-::check if virtualenv is installed
-
-echo Admin permissions are required! Checking..
-NET SESSION >nul 2>&1
-if %errorLevel% == 2 (
-  goto NoAdminRights
-)
-
+::Check if Virtualenv is installed!
 virtualenv --version || :goto VirtualEnvNotInstalled
 echo "Virtualenv is installed!"
 
-::Ask user for python version
-SET /P pyversion=Specify Python Version(2/3)(Default: 3)
-if "%pyversion%"=="" (
-  SET pyversion=3
-)
-echo "You chose %pyversion%"
 
 ::save curred wd as jarvis path
 FOR /F "tokens=*" %%g IN ('chdir') do (SET jarvispath=%%g)
@@ -31,12 +16,7 @@ echo "Jarvispath set as %jarvispath%"
 @echo python %jarvispath%\jarviscli\ >> jarvis.bat
 
 ::create virtualenv
-
-if "%pyversion%"=="2" (
-  virtualenv env --python=python2
-) ELSE (
-  virtualenv env --python=python
-)
+virtualenv env --python=python
 
 ::Disable Virtualenv prompt
 SET VIRTUAL_ENV_DISABLE_PROMT=true
@@ -51,7 +31,7 @@ pip install PyPackages/curses_amd64.whl
 
 echo Setting Path...
 ::make jarvis.bat executable form everywhere ; add it to path
-SETX /M  PATH "%PATH%;%jarvispath%"
+SETX PATH "%PATH%;%jarvispath%"
 
 python -m nltk.downloader -d jarviscli/data/nltk wordnet
 
@@ -59,10 +39,6 @@ goto InstallationSucessful
 
 :VirtualEnvNotInstalled
 echo VirtualEnv needs to be installed!
-PAUSE
-
-:NoAdminRights
-echo You need to run this script as an Admin!
 PAUSE
 
 :InstallationSucessful
