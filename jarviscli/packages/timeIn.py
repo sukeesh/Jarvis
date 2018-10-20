062f1dbc2a4bcb50d273fab87bdefe93b27a2c5a
Arpit Choudhary <arpitkumar147@gmail.com>
2018-10-20 09:03:53 +0530

    apply suggestions

--- jarviscli/packages/timeIn.py
+++ jarviscli/packages/timeIn.py
@@ -7,16 +7,18 @@ from colorama import Fore
 
 # this sets the path to the modules directory not the directory it was call from
 module_path = os.path.dirname(__file__)
+module_path = module_path + '/../data/'
 
 
 def main(self, s):
     # Trims input s to be just the city/region name
     s = s.replace('time ', '').replace('in ', '')
-    exists = os.path.isfile(module_path + '/../data/keys.json')
+
+    exists = os.path.isfile(module_path + 'key_timein.json')
     if not exists:
-        shutil.copy2(module_path + '/../data/samplekeys.json', module_path + '/../data/keys.json')
+        shutil.copy2(module_path + 'samplekey_timein.json', module_path + 'key_timein.json')
         print(Fore.RED + "Generate api key here: https://developers.google.com/maps/documentation/geocoding/start?hl=en_US")
-        print(Fore.RED + "and add it to jarviscli/data/keys.json" + Fore.RESET)
+        print(Fore.RED + "and add it to jarviscli/data/key_timein.json" + Fore.RESET)
         return
 
     # Transforms a city name into coordinates using Google Maps API
@@ -39,12 +41,13 @@ def main(self, s):
 
 
 def getLocation(s):
-    file_path = module_path + '/../data/keys.json'
+    file_path = module_path + 'key_timein.json'
     with open(file_path) as json_file:
         data = json.load(json_file)
     if 'timein' not in data or data['timein'] == 'insertyourkeyhere':
+        print(Fore.RED + "API key not added")
         print(Fore.RED + "Generate api key here: https://developers.google.com/maps/documentation/geocoding/start?hl=en_US")
-        print(Fore.RED + "and add it to jarviscli/data/keys.json" + Fore.RESET)
+        print(Fore.RED + "and add it to jarviscli/data/key_timein.json" + Fore.RESET)
         return None
     key = data['timein']
     send_url = ("https://maps.googleapis.com/maps/api/geocode/json?address={0}&key={1}".format(s, key))
@@ -55,7 +58,7 @@ def getLocation(s):
     try:
         lat = j['results'][0]['geometry']['location']['lat']    # Latitude
         lng = j['results'][0]['geometry']['location']['lng']    # Longitude
-        # Returns both latitude and longitude as a tupple
+        # Returns both latitude and longitude as a tuple
         return lat, lng
     except IndexError:
         print(Fore.RED + "Some error occurred, open an issue on github")
