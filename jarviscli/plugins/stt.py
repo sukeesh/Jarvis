fc29a112eea9b1cb6b82ee8330ea63c26212e423
Mukesh Kumar <mmkumr.ping@gmail.com>
2019-02-10 23:19:21 +0530

    Fixed pyaudio installing problem.

--- jarviscli/plugins/stt.py
+++ jarviscli/plugins/stt.py
@@ -1,22 +1,22 @@
-#!/usr/bin/env python3
 import speech_recognition as sr
 import pyttsx3
 import os
 from plugin import plugin
 @plugin()
 def hear(jarvis, s):
-    r = sr.Recognizer()
+    r = sr.Recognizer()  # intializing the speech_recognition
     engine = pyttsx3.init()
     listen = False
-
+    _jarvis = jarvis._jarvis  # calling jarvis object.
+    _jarvis.speech.text_to_speech("Say listen to start voice mode")
     while listen is False:
         try:
             with sr.Microphone() as source:
-                os.system('reset')
+                os.system('reset')  # for clearing the terminal.
                 print("Say listen to start listening")
-                r.adjust_for_ambient_noise(source)
-                audio = r.listen(source)
-                pinger = r.recognize_google(audio)
+                r.adjust_for_ambient_noise(source)  # Eleminating the noise.
+                audio = r.listen(source)  # Storing audio.
+                pinger = r.recognize_google(audio)  # Converting speech to text
             try:
                 if (pinger.lower() == "listen"):
                     listen = True
@@ -25,9 +25,9 @@ def hear(jarvis, s):
                 else:
                     continue
             except LookupError:
-                continue
+                continue   # For ignoring if your are not speaking anything.
         except sr.UnknownValueError:
-            continue
+            continue  # For ignoring the unreconized words error
 
     while listen is True:
         print("Say somthing")
@@ -40,12 +40,12 @@ def hear(jarvis, s):
             if (pinger == "stop"):
                 listen = False
                 print("Listening stopped.")
+                _jarvis.speech.text_to_speech("Listening stopped.")
                 break
             else:
                 print(pinger)
                 if listen:
                     line = pinger
-                    _jarvis = jarvis._jarvis
                     line = _jarvis.precmd(line)
                     stop = _jarvis.onecmd(line)
                     stop = _jarvis.postcmd(stop, line)
@@ -54,9 +54,9 @@ def hear(jarvis, s):
             engine.say('Audio cannot be read!')
             engine.runAndWait()
             print("Could not understand audio")
+            _jarvis.speech.text_to_speech("unable to recognize voice")
         except sr.UnknownValueError:
-            print("Google Speech Recognition could not understand audio")
             continue
         except sr.RequestError:
             print("Could not request results from Google Recognition service")
-            continue
+            continue  # It will ignore connecting server error.
--- jarviscli/plugins/switchingwin.py
+++ jarviscli/plugins/switchingwin.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python3
 import os
 from plugin import plugin
 @plugin(native='wmctrl')
 def go_to(jarvis, s):
-    os.system("wmctrl -a " + s)
+    os.system("wmctrl -a " + s)  # switch to already opened app/software.
 
 
 @plugin(native='wmctrl')
@@ -11,9 +10,9 @@ def workspace(jarvis, s):
     if s == 'one':
         s = 1
     num = str(int(s) - 1)
-    os.system("wmctrl -s " + num)
+    os.system("wmctrl -s " + num)  # switch workspace.
 
 
 @plugin()
 def run(jarvis, s):
-    os.system(s)
+    os.system(s)  # run the command in terminal.
