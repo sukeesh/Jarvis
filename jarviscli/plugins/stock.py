bd23eae33706740d81d748e1a1a8ba385ec9883c
Konstantinos <kostaspapadopoulos369@gmail.com>
2019-03-19 01:09:30 +0000

    Added error handling for invalid stock symbol

--- jarviscli/plugins/stock.py
+++ jarviscli/plugins/stock.py
@@ -11,12 +11,20 @@ class Stock():
 
 
     def getStockData(self, quote):
-        resp = requests.get('https://api.iextrading.com/1.0/stock/' + quote + '/quote').json()
-        print("Price: " + str(resp['iexRealtimePrice']))
-        print("Open: " + str(resp['open']))
-        print("Close: " + str(resp['close']))
-        print("Change: " + str(resp['changePercent']) + "%")
-        print("MarketCap: " + str(resp['marketCap']))
+
+        resp = requests.get('https://api.iextrading.com/1.0/stock/' + quote + '/quote')
+
+        if (resp.status_code == 200):
+            resp = resp.json()
+            print("Price: " + str(resp['iexRealtimePrice']))
+            print("Open: " + str(resp['open']))
+            print("Close: " + str(resp['close']))
+            print("Change: " + str(resp['changePercent']) + "%")
+            print("MarketCap: " + str(resp['marketCap']))
+        else:
+            print("The given stock symbol could not be found!")
+
+
 
 
 
