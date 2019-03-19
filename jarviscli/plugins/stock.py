1625ef87e1442563c28626f83cf829580ac5a78b
Konstantinos <kostaspapadopoulos369@gmail.com>
2019-03-19 01:00:31 +0000

    Added starting stock functionality plugin

---
+++ jarviscli/plugins/stock.py
@@ -0,0 +1,27 @@
+from plugin import plugin, require
+import requests
+import json
+
+@plugin('stock')
+class Stock():
+
+    def __call__(self, jarvis, s):
+        quote = input("Stock Symbol: ").replace(" ", "")
+        self.getStockData(quote)
+
+
+    def getStockData(self, quote):
+        resp = requests.get('https://api.iextrading.com/1.0/stock/' + quote + '/quote').json()
+        print("Price: " + str(resp['iexRealtimePrice']))
+        print("Open: " + str(resp['open']))
+        print("Close: " + str(resp['close']))
+        print("Change: " + str(resp['changePercent']) + "%")
+        print("MarketCap: " + str(resp['marketCap']))
+
+
+
+
+
+
+# if __name__ == '__main__':
+#     Stock.getStockData(Stock, 'aapl')
