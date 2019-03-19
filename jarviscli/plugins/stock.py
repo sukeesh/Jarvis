364c02f7d78299f57ceca5f1e691ece04eaff0a0
Konstantinos <kostaspapadopoulos369@gmail.com>
2019-03-19 10:50:46 +0000

    Fixed pep8 errors

--- jarviscli/plugins/stock.py
+++ jarviscli/plugins/stock.py
@@ -1,20 +1,20 @@
 from plugin import plugin, require
 import requests
-import json
+
 
 @plugin('stock')
-class Stock():
+class Stock:
 
     def __call__(self, jarvis, s):
         quote = input("Stock Symbol: ").replace(" ", "")
-        self.getStockData(quote)
+        self.get_stock_data(quote)
 
-
-    def getStockData(self, quote):
+    @staticmethod
+    def get_stock_data(quote):
 
         resp = requests.get('https://api.iextrading.com/1.0/stock/' + quote + '/quote')
 
-        if (resp.status_code == 200):
+        if resp.status_code == 200:
             resp = resp.json()
             print("Price: " + str(resp['iexRealtimePrice']))
             print("Open: " + str(resp['open']))
@@ -23,13 +23,3 @@ class Stock():
             print("MarketCap: " + str(resp['marketCap']))
         else:
             print("The given stock symbol could not be found!")
-
-
-
-
-
-
-
-
-# if __name__ == '__main__':
-#     Stock.getStockData(Stock, 'aapl')
