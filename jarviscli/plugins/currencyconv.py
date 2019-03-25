01a116c19b95124eea30273eccbcb42d63a5560a
Jon Ander Oribe <jonan.oribe@gmail.com>
2019-03-25 19:47:51 +0100

    Adding a more verbosity explanation about the CURRENCY CONVERTER output
    Now its gives you something like: "100.0 AUD are equal to 62.7700 EUR" as response, instead of only "62.7700"

--- jarviscli/plugins/currencyconv.py
+++ jarviscli/plugins/currencyconv.py
@@ -46,8 +46,9 @@ class Currencyconv():
             result = b.convert_btc_to_cur(Decimal(amount), to)
         else:
             result = c.convert(fr, to, Decimal(amount))
-
-        jarvis.say(str(amount)+" "+fr+" are equal to "+str(result)+" "+to)
+        
+        outputText=str(amount)+" "+fr+" are equal to "+str(result)+" "+to
+        jarvis.say(outputText)
 
     def find_currencies(self):
         """
