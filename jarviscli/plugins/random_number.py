0933f101149ef207c0d5032daac7a00b3e051aa8
= <rogeramorinvieiraa@gmail.com>
2019-04-26 16:48:47 -0300

    make the functions more modular

--- jarviscli/plugins/random_number.py
+++ jarviscli/plugins/random_number.py
@@ -2,23 +2,47 @@ from plugin import plugin
 import random
 
 @plugin("random number")
-def random_password(jarvis, s):
-    stringFail = True
+def generate_random_number(jarvis, s):
 
-    while (stringFail):
+    if s != "":
+        input_numbers = s.split(' ', 1)
+
         try:
-            smallest_number = int(input("Enter the smallest number possible: "))
-            higher_number = int(input("Enter the higher number possible: "))
-            stringFail = False
+            smallest_number = int(input_numbers[0])
+            higher_number = int(input_numbers[1])
         except:
-            print('Only integers will be accepted')
+            print('Values are invalid please:')
+            smallest_number, higher_number = get_user_input()
+    else:
+        smallest_number, higher_number = get_user_input()
 
     """If the user change the order of input"""
-    if(higher_number < smallest_number):
+    if higher_number < smallest_number:
         aux = higher_number
         higher_number = smallest_number
         smallest_number = aux
 
-    preText = "Your random number is";
-    random_number = random.randint(smallest_number,higher_number+1)
-    print(preText, random_number)
+    pre_text = 'Your random number in range [%d, %d] is' % (smallest_number, higher_number);
+    rand_number = random.randint(smallest_number, higher_number)
+    print(pre_text, rand_number)
+
+
+def get_user_input():
+    string_fail = True
+    while string_fail:
+        try:
+            smallest_number = int(input('Enter the smallest number possible: '))
+            string_fail = False
+        except:
+            print('Only integers will be accepted')
+
+    string_fail = True
+
+    while string_fail:
+        try:
+            higher_number = int(input('Enter the higher number possible: '))
+            string_fail = False
+        except:
+            print('Only integers will be accepted')
+
+    return smallest_number,higher_number;
