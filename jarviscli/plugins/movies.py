41fc4302095bed04bce9ee61f74c5111d9883d17
pnhofmann <foss@pnhofmann.de>
2019-05-14 16:46:56 +0200

    Fix typo

--- jarviscli/plugins/movies.py
+++ jarviscli/plugins/movies.py
@@ -5,7 +5,7 @@ from plugin import plugin, require, LINUX
 
 @require(platform=LINUX, native='ims')
 @plugin('movies')
-def movies():
+def movies(jarvis, s):
     """Jarvis will find a good movie for you"""
     movie_name = jarvis.input("What do you want to watch?\n", Fore.RED)
     os.system("ims " + movie_name)
