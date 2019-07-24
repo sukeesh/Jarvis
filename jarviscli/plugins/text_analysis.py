3a6f3e0f051f50b433175d33371efe0da7983e79
Jon Ander Oribe <jonan.oribe@gmail.com>
2019-07-25 00:38:02 +0200

    Update text_analysis.py

--- jarviscli/plugins/text_analysis.py
+++ jarviscli/plugins/text_analysis.py
@@ -6,7 +6,7 @@ nltk.download('averaged_perceptron_tagger')
 nltk.download('maxent_ne_chunker')
 nltk.download('words')
 
-from nltk import word_tokenize,sent_tokenize
+from nltk import word_tokenize, sent_tokenize
 
 
 @plugin("text analysis")
@@ -18,4 +18,3 @@ def text_analysis(jarvis, s):
     entities = nltk.chunk.ne_chunk(tagged)
     print('Analysis result: ')
     print(entities)
-
