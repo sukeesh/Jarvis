aae92345df3887b117b44b35ebcb4dbeffd6bcbf
Jon Ander Oribe <jonan.oribe@gmail.com>
2019-07-25 00:52:07 +0200

    Update text_analysis.py

--- jarviscli/plugins/text_analysis.py
+++ jarviscli/plugins/text_analysis.py
@@ -7,9 +7,9 @@ nltk.download('averaged_perceptron_tagger')
 nltk.download('maxent_ne_chunker')
 nltk.download('words')
 
+
 @plugin("text analysis")
 def text_analysis(jarvis, s):
-
     sentence = jarvis.input("Write down the text: ")
     tokens = nltk.word_tokenize(sentence)
     tagged = nltk.pos_tag(tokens)
