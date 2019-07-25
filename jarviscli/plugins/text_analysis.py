044a4810d2d1333e6efc1865076f914533bb81f4
Jon Ander Oribe <jonan.oribe@gmail.com>
2019-07-25 12:31:32 +0200

    Update text_analysis.py

--- jarviscli/plugins/text_analysis.py
+++ jarviscli/plugins/text_analysis.py
@@ -10,6 +10,10 @@ nltk.download('words')
 
 @plugin("text analysis")
 def text_analysis(jarvis, s):
+    """
+    Basic functionallity for a NLTK system.
+    Just write a string and the script will give you the entities that it can find
+    """
     sentence = jarvis.input("Write down the text: ")
     tokens = nltk.word_tokenize(sentence)
     tagged = nltk.pos_tag(tokens)
