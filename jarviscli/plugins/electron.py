96ae41977e6683de50fb1776701fb6b545e449af
Jon Ander Oribe <jonan.oribe@gmail.com>
2019-07-26 23:19:41 +0200

    Update electron.py

--- jarviscli/plugins/electron.py
+++ jarviscli/plugins/electron.py
@@ -4,6 +4,7 @@ from npm.bindings import npm_run
 
 @plugin("electron")
 def electron(jarvis, s):
-    #Open a browser for creating a Jarvis GUI with HTML,CSS and Javascript
+    # Open a browser for creating a Jarvis GUI with HTML,CSS and Javascript
     print("Loading...")
     pkg = npm_run('start')
+    
