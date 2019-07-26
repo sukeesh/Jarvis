8709c9d70539a919126c8adce1f45f1bef4eda87
Jon Ander Oribe <jonan.oribe@gmail.com>
2019-07-26 23:03:12 +0200

    Jarvis basic GUI
    The first step on a GUI  with Electron and Node

---
+++ jarviscli/plugins/electron.py
@@ -0,0 +1,9 @@
+from plugin import plugin
+from npm.bindings import npm_run
+
+
+@plugin("electron")
+def electron(jarvis, s):
+    #Open a browser for creating a Jarvis GUI with HTML,CSS and Javascript
+    print("Loading...")
+    pkg = npm_run('start')
