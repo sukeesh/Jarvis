ada9a23fac3f1e84d5bee7ccd4edec7db31995a4
Matthew237 <matthew.macintosh@btinternet.com>
2019-07-26 15:12:19 -0700

    Forgot to run the PEP-8 Test
    All code should now follow the guidelines

--- jarviscli/plugins/translate.py
+++ jarviscli/plugins/translate.py
@@ -10,20 +10,19 @@ import nltk
 def translate(jarvis, s):
     """
     Translates from one language to another and allows input to be somewhat natural.
-    
-    Usage: 
-    
+
+    Usage:
+
     'Jarvis, please translate, from English to French, Hello, how are you?'
-    
+
     OR
-    
+
     'Jarvis, could you translate Hello, how are you? from English to French for me please?'
     """
-    
+
 #   Check whether user has entered translate by itself or with extra parameters
     if s != "":
         words = nltk.word_tokenize(s.lower())
-        
         currentPos = 0
         finalPos = 0
         srcs = None
@@ -33,15 +32,16 @@ def translate(jarvis, s):
         for i in range(len(words)):
             word = words[i]
             currentPos = i
-            
-#           Do not include language codes in the tests when using full sentence command since words can conflict with them (Eg. hi -> Hindi).
-#           This code looks like it includes them, but since the googletrans API is implemented such that the languages are stored in dictionaries,
-#           when the "in" operator is used, it only checks the keys of the dictionary, not the values. Therefore, the LANG_CODES dictionary must be
-#           used to check full language names instead of the LANGUAGES dictionary. For more clarification, have a look at the code on the googletrans github.
+
+#           Do not include lang codes in the tests when using full sentence command since words can conflict with them (Eg. hi -> Hindi).
+#           This code looks like it includes them, but since the googletrans API is implemented such that the languages are stored in
+#           dictionaries, when the "in" operator is used, it only checks the keys of the dictionary, not the values. Therefore, the
+#           LANG_CODES dictionary must be used to check full language names instead of the LANGUAGES dictionary. For more clarification,
+#           have a look at the code on the googletrans github.
             if (word in LANGCODES):
                 srcs = word
                 break
-        
+
 #       Search input string for destination language starting from the word after the source language
         for i in range(currentPos + 1, len(words)):
             word = words[i]
