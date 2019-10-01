5008dbc3f32073efc2f78757faab86d4fc5278ef
Arpit Choudhary <choudhary_arpit@optum.com>
2019-10-01 16:04:51 +0530

    add battery

--- jarviscli/plugins/battery.py
+++ jarviscli/plugins/battery.py
@@ -1,12 +1,32 @@
 import subprocess
-from plugin import plugin, require, LINUX
+from plugin import plugin, require, LINUX, WINDOWS
 
 VALID_OPTIONS = ['status', 'vendor', 'energy', 'technology', 'remaining']
 
 
+@require(platform=WINDOWS)
+@plugin('battery')
+def battery_WIN32(jarvis, s):
+    """
+    Provides basic info about battery fo win32
+    """
+    # https://stackoverflow.com/a/41988506/6771356
+    import psutil
+    def secs2hours(secs):
+        mm, ss = divmod(secs, 60)
+        hh, mm = divmod(mm, 60)
+        return "%d:%02d:%02d" % (hh, mm, ss)
+
+    batt = psutil.sensors_battery()
+    if batt.power_plugged:
+        jarvis.say("Battery is charging: %s%%" % batt.percent)
+    else:
+        jarvis.say("charge = %s%%, time left = %s" % (batt.percent, secs2hours(batt.secsleft)))
+
+
 @require(platform=LINUX, native='upower')
 @plugin('battery')
-def battery(jarvis, s):
+def battery_LINUX(jarvis, s):
     """
     Provides battery status like battery percentage
     and if the battery is charging or not
