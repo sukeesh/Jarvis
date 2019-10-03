b22f532b0a340f9465d906730102b9547c290899
Akshath Kaushal <42678785+akshathkaushal@users.noreply.github.com>
2019-10-03 16:50:57 +0530

    Add files via upload

---
+++ jarviscli/plugins/open_url.py
@@ -0,0 +1,16 @@
+from plugin import plugin
+import urllib.request
+from colorama import Fore
+import webbrowser
+
+@plugin('open link')
+def openUrl(jarvis,s):
+	jarvis.say("Please enter a url: ", Fore.WHITE)
+	url = input()
+	jarvis.say("Opening the url....",Fore.GREEN)
+	url1 = "https://"+url
+	try:
+		webbrowser.open(url1)
+	except:
+		url2 = "http://"+url
+		webbrowser.open(url2) 
