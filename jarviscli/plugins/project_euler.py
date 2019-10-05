22023483dcc0ef6f9c6eb6df02252f36f1303494
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2019-10-05 23:09:02 +0300

    euler plugin: a prototype of menu

--- jarviscli/plugins/project_euler.py
+++ jarviscli/plugins/project_euler.py
@@ -6,4 +6,11 @@ import requests
 @plugin('euler')
 class Euler():
     def __call__(self, jarvis, s):
-        jarvis.say('Euler')
+        jarvis.say('Welcome to the ProjectEuler plugin!', Fore.GREEN)
+        jarvis.say('Please select an option below:')
+        jarvis.say('')
+        jarvis.say('1) Get the problem by number')
+        jarvis.say('2) Get random problem')
+        choice = jarvis.input_number('Your choice: ')
+        
+        print(choice)
