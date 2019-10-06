d1817746a1fdfe03cc606c0cb071989b53f2ac8e
James <jwin4740@gmail.com>
2019-10-05 22:59:43 -0400

    added typing game skeleton

---
+++ jarviscli/packages/typinggame.py
@@ -0,0 +1,15 @@
+# skeleton for a typing speed game
+
+
+class TypingGame:
+    paragraph = None
+
+    def __init__(self, category):
+        self.category = category
+
+    # method to retrieve relevant paragraph
+    def retrieve_paragraph(self):
+        pass
+
+    def start_timer(self):
+        pass
