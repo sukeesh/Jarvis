cf0d2d144c0d1d793a8c6cd0b176e39ee6675185
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2019-10-06 18:16:29 +0300

    euler plugin: 'get problem by number'- the title is shown

--- jarviscli/plugins/project_euler.py
+++ jarviscli/plugins/project_euler.py
@@ -2,15 +2,50 @@ from plugin import plugin, alias, require
 from colorama import Fore
 import requests
 
+from bs4 import BeautifulSoup
+
+
+
 @require(network=True)
 @plugin('euler')
 class Euler():
+
+    def __init__(self):
+        self.project_url = 'https://www.projecteuler.net'
+
     def __call__(self, jarvis, s):
-        jarvis.say('Welcome to the ProjectEuler plugin!', Fore.GREEN)
-        jarvis.say('Please select an option below:')
-        jarvis.say('')
-        jarvis.say('1) Get the problem by number')
-        jarvis.say('2) Get random problem')
-        choice = jarvis.input_number('Your choice: ')
-        
-        print(choice)
+
+        self.jarvis = jarvis
+
+        self.jarvis.say('Welcome to the ProjectEuler plugin!', Fore.GREEN)
+        self.jarvis.say('-----------------------------------', Fore.GREEN)
+        self.jarvis.say('')
+        self.jarvis.say('Please select an option below:')
+        self.jarvis.say('1) Get the problem by number')
+        self.jarvis.say('2) Get random problem')
+        choice = self.jarvis.input_number('Your choice: ', rtype=int)
+
+        if choice == 1:
+            problem_number = self.jarvis.input_number('Please, enter the desired number: ', rtype=int)
+            self.get_problem_by_number(problem_number)
+
+
+    def get_problem_by_number(self, number):
+
+        # Form link depending on the number of the task and get the page via requests module
+        url = self.project_url + '/problem=' + str(number)
+        page = requests.get(url)
+
+        # Use bs4 to parse the page
+        soup = BeautifulSoup(page.content, 'html.parser')
+
+        # We are only interested in the block with id 'content'
+        content = soup.find('div', id='content')
+
+        # Get the title of the problem and print it
+        problem_header = content.find('h2').get_text()
+        header_to_show = 'Problem ' + str(number) + '. ' + problem_header
+        self.jarvis.say('')
+        self.jarvis.say(header_to_show, Fore.GREEN)
+
+
