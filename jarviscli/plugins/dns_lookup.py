0a4da373ec0e3fc1a24338827ede252d196dba73
menachem <mkossowsky42@gmail.com>
2019-10-07 14:24:07 +0300

    restructerd plugin and added .vscode to gitignore

--- jarviscli/plugins/dns_lookup.py
+++ jarviscli/plugins/dns_lookup.py
@@ -3,49 +3,41 @@ import socket
 from colorama import Fore
 
 
-# a reverse DNS lookup or reverse DNS resolution (rDNS)
-# is the querying technique of the Domain Name System (DNS)
-# to determine the domain name associated with an IP address
-@plugin("dns reverse")
-def hostname_lookup(jarvis, s):
+def ip_lookup(hostname):
+    return str(socket.gethostbyname(hostname))
+
+
+def hostname_lookup(ip):
+    return str(socket.gethostbyaddr(ip)[0])
+
+
+def dns_lookup(jarvis, s, txt, func):
     while True:
-        # asks for an IP address
-        ip = str(jarvis.input("Please input an IP address: "))
+        request = str(jarvis.input("Please input a " + txt + ": "))
         try:
-            # tries to find the hostname for given IP address
-            jarvis.say("The hostname for that IP address is: " +
-                       str(socket.gethostbyaddr(ip)[0]), Fore.BLUE)
-            break
+            if txt == 'ip':
+                jarvis.say("The hostname for that IP address is: " +
+                           func(request), Fore.CYAN)
+                return
+            else:
+                jarvis.say("The IP address for that hostname is: " +
+                           func(request), Fore.CYAN)
+                return
         except Exception as e:
-            # prints error and asks if want to try again
             jarvis.say(str(e), Fore.RED)
-            jarvis.say("Please make sure you are inputing a valid IP address")
+            jarvis.say("Please make sure you are inputing a valid " + txt)
             try_again = jarvis.input("Do you want to try again (y/n): ")
             try_again = try_again.lower()
-            if try_again == 'y':
-                continue
-            else:
-                break
-
-# a "forward" DNS lookup is a lookup of an IP address from a domain name.
+            if try_again != 'y':
+                return
 
 
 @plugin("dns forward")
-def ip_lookup(jarvis, s):
-    while True:
-        hostname = str(jarvis.input("Please input a hostname: "))
-        try:
-            # tries to find the hostname for given hostname
-            jarvis.say("The IP address for that hostname is: " +
-                       str(socket.gethostbyname(hostname)), Fore.BLUE)
-            break
-        except Exception as e:
-            # prints error and asks if want to try again
-            jarvis.say(str(e), Fore.RED)
-            jarvis.say("Please make sure you are inputing a valid IP address")
-            try_again = jarvis.input("Do you want to try again (y/n): ")
-            try_again = try_again.lower()
-            if try_again == 'y':
-                continue
-            else:
-                break
+def ip_lookup1(jarvis, s):
+    dns_lookup(jarvis, s, "hostname", ip_lookup)
+
+
+@plugin("dns reverse")
+def hostname_lookup1(jarvis, s):
+    dns_lookup(jarvis, s, "ip", hostname_lookup)
+
