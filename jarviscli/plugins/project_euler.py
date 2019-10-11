ce892d23d4fb51c98864cb075768cf7637e75bc0
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2019-10-11 23:44:55 +0300

    euler plugin: add the nomber of problems to info message

--- jarviscli/plugins/project_euler.py
+++ jarviscli/plugins/project_euler.py
@@ -101,7 +101,7 @@ class Euler():
         info_text = "Project Euler (named after Leonhard Euler) is a website dedicated to a series of computational problems intended to be solved with computer programs. "
         info_text += "The project attracts adults and students interested in mathematics and computer programming. "
         info_text += "Since its creation in 2001 by Colin Hughes, Project Euler has gained notability and popularity worldwide. "
-        info_text += "It includes over 670 problems, with a new one added once every one or two weeks. "
+        info_text += "It now includes " + str(self.last_problem_id) + " problems. A new one is added once every one or two weeks. "
         info_text += "Problems are of varying difficulty, but each is solvable in less than a minute of CPU time using an efficient algorithm on a modestly powered computer. "
 
         self.jarvis.say(info_text)
