6bbd7f65a85b607fd5b24895d6dbbdd2c1bca017
theViz343 <vishwesh103@gmail.com>
2019-10-21 10:28:02 +0530

    added handling for bad input

--- jarviscli/plugins/blackjack.py
+++ jarviscli/plugins/blackjack.py
@@ -9,12 +9,11 @@ def clear_lines(number=1):
         sys.stdout.write(ERASE_LINE)
 def delay():
     n=input("Press enter to continue")
-def wiped_slate(players):
-    for player in players:
-        player['hands']=[]
-        player['suits']=[]
-        player['bets']=[]
-    return players
+def wiped_slate(player):
+    player['hands']=[]
+    player['suits']=[]
+    player['bets']=[]
+    return player
 def pprinthand(hand,suit,type='visible'):
     temphand=hand[:]
     for i in range(len(temphand)):
@@ -56,90 +55,90 @@ def move(hand,suit,cards,suits,bet):# Here, hand is a nested list inside a list.
     elif sum_==21 and len(hand)==2:
         print("Blackjack!")
         return hand,suit, bet
-    choice=input("Press H to Hit, S to Stand, D to Double-Down, P to sPlit")
-
-    if choice in['H','h']:
-        newcard=random.choice(cards)
-        newsuit = random.choice(suits)
-        print("Newcard is",str(newcard)+" of "+newsuit)
-        hand[0].append(newcard)
-        suit[0].append(newsuit)
-        print("Updated hand is",pprinthand(hand[0],suit[0]))
-        sum_, hand[0] = blackjacksum(hand[0])
-        hand,suit,bet=move(hand,suit,cards,suits,bet)
-        return hand,suit,bet
-
-    if choice in['S','s']:
-        return hand,suit,bet
-
-    if choice in['D','d']:
-        newcard = random.choice(cards)
-        print("Newcard is", newcard)
-        newsuit = random.choice(suits)
-        hand[0].append(newcard)
-        suit[0].append(newsuit)
-        print("Updated hand is", pprinthand(hand[0],suit[0]))
-        sum_, hand[0] = blackjacksum(hand[0])
-        print("Your sum is", sum_)
-        if sum_ > 21:
-            print("You got busted!")
-        bet[0]=bet[0]*2
-        print("Your new bet is", bet[0])
-        return hand,suit,bet
-
-    if choice in['P','p']:
-        if hand[0][0]==hand[0][1]:
-            if not hand[0][0]==1:
-                splitHand1=[[0,0]]
-                splitHand2=[[0,0]]
-                splitSuit1 = [[0, 0]]
-                splitSuit2 = [[0, 0]]
-                newcard1=random.choice(cards)
-                newsuit1 = random.choice(suits)
-                print("Newcard for first split is",str(newcard1)+" of "+newsuit1)
-                newcard2 = random.choice(cards)
-                newsuit2 = random.choice(suits)
-                print("Newcard for second split is", str(newcard2)+" of "+newsuit2)
-                splitHand1[0][0] = hand[0][0]
-                splitHand2[0][0] = hand[0][1]
-                splitHand1[0][1] = newcard1
-                splitHand2[0][1] = newcard2
-                splitSuit1[0][0] = suit[0][0]
-                splitSuit2[0][0] = suit[0][1]
-                splitSuit1[0][1] = newsuit1
-                splitSuit2[0][1] = newsuit2
-                print("Split hands are",pprinthand(splitHand1[0],splitSuit1[0]),", ",pprinthand(splitHand2[0],splitSuit2[0]))
-                sum1,splitHand1[0] = blackjacksum(splitHand1[0])
-                sum2, splitHand2[0] = blackjacksum(splitHand2[0])
-                print("Your sum for split 1 is", sum1)
-                print("Your sum for split 2 is", sum2)
-                bet1=bet[:]
-                bet2 = bet[:]
-                splitHand1,splitSuit1,bet1=move(splitHand1,splitSuit1,cards,suits,bet1)
-                splitHand2,splitSuit2,bet2=move(splitHand2,splitSuit2,cards,suits,bet2)
-                splitHand1.extend(splitHand2)#converting both hands to a single list
-                splitSuit1.extend(splitSuit2)
-                bet1.extend(bet2)#converting both bets to a single list
-                return splitHand1,splitSuit1,bet1
-            else:
-                print("Sorry,you can't split aces")
+    while True:
+        choice = input("Press H to Hit, S to Stand, D to Double-Down, P to sPlit")
+        try:
+            if choice in['H','h']:
+                newcard=random.choice(cards)
+                newsuit = random.choice(suits)
+                print("Newcard is",str(newcard)+" of "+newsuit)
+                hand[0].append(newcard)
+                suit[0].append(newsuit)
+                print("Updated hand is",pprinthand(hand[0],suit[0]))
+                sum_, hand[0] = blackjacksum(hand[0])
                 hand,suit,bet=move(hand,suit,cards,suits,bet)
                 return hand,suit,bet
-        else:
-            print("Sorry, you can only split hands with identical cards")
-            hand,suit, bet = move(hand,suit, cards,suits, bet)
-            return hand,suit, bet
+
+            elif choice in['S','s']:
+                return hand,suit,bet
+
+            elif choice in['D','d']:
+                newcard = random.choice(cards)
+                print("Newcard is", newcard)
+                newsuit = random.choice(suits)
+                hand[0].append(newcard)
+                suit[0].append(newsuit)
+                print("Updated hand is", pprinthand(hand[0],suit[0]))
+                sum_, hand[0] = blackjacksum(hand[0])
+                print("Your sum is", sum_)
+                if sum_ > 21:
+                    print("You got busted!")
+                bet[0]=bet[0]*2
+                print("Your new bet is", bet[0])
+                return hand,suit,bet
+
+            elif choice in['P','p']:
+                if hand[0][0]==hand[0][1]:
+                    if not hand[0][0]==1:
+                        splitHand1=[[0,0]]
+                        splitHand2=[[0,0]]
+                        splitSuit1 = [[0, 0]]
+                        splitSuit2 = [[0, 0]]
+                        newcard1=random.choice(cards)
+                        newsuit1 = random.choice(suits)
+                        print("Newcard for first split is",str(newcard1)+" of "+newsuit1)
+                        newcard2 = random.choice(cards)
+                        newsuit2 = random.choice(suits)
+                        print("Newcard for second split is", str(newcard2)+" of "+newsuit2)
+                        splitHand1[0][0] = hand[0][0]
+                        splitHand2[0][0] = hand[0][1]
+                        splitHand1[0][1] = newcard1
+                        splitHand2[0][1] = newcard2
+                        splitSuit1[0][0] = suit[0][0]
+                        splitSuit2[0][0] = suit[0][1]
+                        splitSuit1[0][1] = newsuit1
+                        splitSuit2[0][1] = newsuit2
+                        print("Split hands are",pprinthand(splitHand1[0],splitSuit1[0]),", ",pprinthand(splitHand2[0],splitSuit2[0]))
+                        sum1,splitHand1[0] = blackjacksum(splitHand1[0])
+                        sum2, splitHand2[0] = blackjacksum(splitHand2[0])
+                        print("Your sum for split 1 is", sum1)
+                        print("Your sum for split 2 is", sum2)
+                        bet1=bet[:]
+                        bet2 = bet[:]
+                        splitHand1,splitSuit1,bet1=move(splitHand1,splitSuit1,cards,suits,bet1)
+                        splitHand2,splitSuit2,bet2=move(splitHand2,splitSuit2,cards,suits,bet2)
+                        splitHand1.extend(splitHand2)#converting both hands to a single list
+                        splitSuit1.extend(splitSuit2)
+                        bet1.extend(bet2)#converting both bets to a single list
+                        return splitHand1,splitSuit1,bet1
+                    else:
+                        print("Sorry,you can't split aces")
+                        hand,suit,bet=move(hand,suit,cards,suits,bet)
+                        return hand,suit,bet
+                else:
+                    print("Sorry, you can only split hands with identical cards")
+                    hand,suit, bet = move(hand,suit, cards,suits, bet)
+                    return hand,suit, bet
+            else:
+                raise Exception
+        except:
+            print("Please try again with a valid choice.")
 ############################################################################
 # # Main driver code
 @plugin('blackjack')
 def blackjack(jarvis,s):
     jarvis.say("Welcome to the casino! Let's play blackjack!")
-    n=jarvis.input_number("How many players are playing?",rtype=int)
-    players=[]
-    dealerhand=[]
-    for i in range(n):
-        name=jarvis.input("Enter name, Player{}".format(i+1))
-        players.append({"name":name,"hands":[],"suits":[],"bets":[],'profit':[]})
+    player={"hands":[],"suits":[],"bets":[],'profit':[]}
     cards=[1,2,3,4,5,6,7,8,9,10,10,10,10]
     suits=['spades','hearts','diamonds','clubs']
     choice='y'
@@ -147,19 +146,18 @@ def blackjack(jarvis,s):
     while choice in "Yy":
         jarvis.say("Let's start the game!")
         ########### Bets
-        for i in range(n):
-            jarvis.say(players[i]["name"],",How much are you betting?")
-            bet=jarvis.input()
-            players[i]["bets"].append(bet)
+        jarvis.say("How much are you betting?")
+        bet=jarvis.input_number()
+        player['bets'].append(bet)
+        delay()
         ########### Cards
         jarvis.say("Dealing the cards............")
-        for i in range(n):
-            jarvis.say(players[i]["name"],"Your cards....")
-            hand=[random.choice(cards),random.choice(cards)]
-            suit=[random.choice(suits),random.choice(suits)]
-            players[i]["hands"].append(hand)
-            players[i]["suits"].append(suit)
-            jarvis.say(pprinthand(hand,suit))
+        jarvis.say("Your cards....")
+        hand=[random.choice(cards),random.choice(cards)]
+        suit=[random.choice(suits),random.choice(suits)]
+        player["hands"].append(hand)
+        player["suits"].append(suit)
+        jarvis.say(pprinthand(hand,suit))
         delay()
         ########### Dealer's cards
         dealerhand=[random.choice(cards),random.choice(cards)]
@@ -167,12 +165,11 @@ def blackjack(jarvis,s):
         jarvis.say("Dealer hand: "+pprinthand(dealerhand,dealersuit,type='partially-visible'))
         delay()
         ########### Players' moves
-        for i in range(n):
-            jarvis.say("It's your turn, "+ players[i]['name'])
-            players[i]['hands'],players[i]['suits'],players[i]['bets']=move(players[i]['hands'],players[i]['suits'],cards,suits,players[i]['bets'])
-            jarvis.say("Your hands and respective bets for this round are:")
-            jarvis.say(pprinthandlist(players[i]['hands'],players[i]['suits'])+"      "+str(players[i]['bets']))
-            delay()
+        jarvis.say("It's your turn, make your choice!")
+        player['hands'],player['suits'],player['bets']=move(player['hands'],player['suits'],cards,suits,player['bets'])
+        jarvis.say("Your hands and respective bets for this round are:")
+        jarvis.say(pprinthandlist(player['hands'],player['suits'])+"      "+str(player['bets']))
+        delay()
         ########### Dealer's moves
         jarvis.say("Dealer hand: "+pprinthand(dealerhand,dealersuit))
         dealersum,dealerhand=blackjacksum(dealerhand)
@@ -187,55 +184,53 @@ def blackjack(jarvis,s):
             jarvis.say("Dealer's hand is "+ pprinthand(dealerhand,dealersuit))
         delay()
         ########### Profit Calculation
-        for i in range(n):
-            jarvis.say('---------------------------')
-            jarvis.say("Let's see your results "+players[i]['name'])
-            for j in range(len(players[i]['hands'])):
-                hand=players[i]['hands'][j]
-                suit=players[i]['suits'][j]
-                bet=players[i]['bets'][j]
-                sum_,hand=blackjacksum(hand)
-                dealersum,dealerhand=blackjacksum(dealerhand)
-                jarvis.say("For the hand- "+pprinthand(hand,suit)+'  sum is-'+str(sum_))
-                if len(hand)==2 and sum_==21:
-                    jarvis.say("Blackjack!")
-                    profit=bet*1.5
-                    players[i]['profit'].append(bet*1.5)
-                elif sum_>21:
-                    jarvis.say("Busted")
-                    profit = bet * -1
-                    players[i]['profit'].append(bet * -1)
-                elif dealersum>21:
-                    jarvis.say("Dealer Busted")
-                    profit = bet * 1
-                    players[i]['profit'].append(bet*1)
-                elif dealersum>sum_:
-                    jarvis.say("You lost")
-                    profit = bet * -1
-                    players[i]['profit'].append(bet * -1)
-                elif sum_>dealersum:
-                    jarvis.say("You win")
-                    profit = bet * 1
-                    players[i]['profit'].append(bet * 1)
-                elif sum_==21 and dealersum==21 and len(dealerhand)==2 and len(hand)>2:
-                    jarvis.say("You lost")
-                    profit = bet * -1
-                    players[i]['profit'].append(bet * -1)
-                elif sum_==dealersum:
-                    jarvis.say("Push")
-                    profit = bet * 0
-                    players[i]['profit'].append(bet * 0)
-                jarvis.say("Profit is- "+str(profit))
-        players=wiped_slate(players)
+        jarvis.say('---------------------------')
+        jarvis.say("Let's see your results ")
+        for j in range(len(player['hands'])):
+            hand=player['hands'][j]
+            suit=player['suits'][j]
+            bet=player['bets'][j]
+            sum_,hand=blackjacksum(hand)
+            dealersum,dealerhand=blackjacksum(dealerhand)
+            jarvis.say("For the hand- "+pprinthand(hand,suit)+'  sum is-'+str(sum_))
+            if len(hand)==2 and sum_==21:
+                jarvis.say("Blackjack!")
+                profit=bet*1.5
+                player['profit'].append(bet*1.5)
+            elif sum_>21:
+                jarvis.say("Busted")
+                profit = bet * -1
+                player['profit'].append(bet * -1)
+            elif dealersum>21:
+                jarvis.say("Dealer Busted")
+                profit = bet * 1
+                player['profit'].append(bet*1)
+            elif dealersum>sum_:
+                jarvis.say("You lost")
+                profit = bet * -1
+                player['profit'].append(bet * -1)
+            elif sum_>dealersum:
+                jarvis.say("You win")
+                profit = bet * 1
+                player['profit'].append(bet * 1)
+            elif sum_==21 and dealersum==21 and len(dealerhand)==2 and len(hand)>2:
+                jarvis.say("You lost")
+                profit = bet * -1
+                player['profit'].append(bet * -1)
+            elif sum_==dealersum:
+                jarvis.say("Push")
+                profit = bet * 0
+                player['profit'].append(bet * 0)
+            jarvis.say("Profit is- "+str(profit))
+        players=wiped_slate(player)
         choice=jarvis.input("Do you wish to play another round?Y/n")
     jarvis.say("OK then, Let's see the results")
-    for i in range(n):                     # total profit calculation
-        name=players[i]['name']
-        profit=sum(players[i]['profit'])
-        if profit>=0:
-            jarvis.say(name+" , your total profit is "+str(profit))
-        else:
-            jarvis.say(name+  " , your total loss is "+ str(profit * -1))
+    # total profit calculation
+    profit=sum(player['profit'])
+    if profit>=0:
+        jarvis.say("Your total profit is "+str(profit))
+    else:
+        jarvis.say("Your total loss is "+ str(profit * -1))
 
 
 
