a8c397e17e88335795377e5ee86e5c7e207aaa28
Quanta-of-solitude <sanjeevboo023@gmail.com>
2019-10-26 01:00:07 +0530

    mp_downloader

--- jarviscli/plugins/mp3download.py
+++ jarviscli/plugins/mp3download.py
@@ -10,7 +10,7 @@ def mp3download(jarvis):
 
     jarvis.say("Enter the name of the music")
     query = jarvis.input()
-    query_string = urllib.parse.urlencode({"search_query" : "{}".format(query)})
+    query_string = urllib.parse.urlencode({"search_query": "{}".format(query)})
     html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
     search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
     url = "http://www.youtube.com/watch?v=" + search_results[0]
@@ -19,12 +19,13 @@ def mp3download(jarvis):
 
         'format': 'bestaudio/best',
         'outtmpl': '%(title)s.%(ext)s',
-        'postprocessors' : [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '192'
+        'postprocessors':
+        [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192'
         }],
-        }
+                }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download(["{}".format(url)])
         res = ydl.extract_info("{}".format(url))
