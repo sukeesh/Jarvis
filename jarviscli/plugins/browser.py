6770b2ad9c19cd74ded7038a8b030957a0b8e560
akshgpt7 <akshgpt7@gmail.com>
2019-11-03 03:54:00 +0530

    added browser plugin

---
+++ jarviscli/plugins/browser.py
@@ -0,0 +1,23 @@
+from plugin import plugin, require
+import webbrowser
+from colorama import Fore
+
+
+@require(network=True)
+@plugin('browser')
+def browser(jarvis,s):
+    """
+    Command 'browser' opens Google search in a new browser window.
+    If you want to open a new tab in an existing browser window, write 'browser new_tab'.
+    (If there is no existing window, new window is opened.)
+    """
+
+    if "new_tab" in s:
+        """Opens Google search in a new tab if a browser window is already opened, otherwise in new window."""
+        webbrowser.open_new_tab('https://www.google.com')
+        jarvis.say("Opened new tab in existing browser window.", Fore.BLUE)
+
+    else:
+        """Opens Google search in a new browser window."""
+        webbrowser.open_new('https://www.google.com')
+        jarvis.say("Opened new browser window.", Fore.BLUE)
