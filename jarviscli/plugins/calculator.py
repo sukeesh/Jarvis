c7a2adcedf31ce8c90fd97703521cf83ccfa1c09
akshgpt7 <akshgpt7@gmail.com>
2019-11-11 06:26:16 +0530

    fixed hanging indents

--- jarviscli/plugins/calculator.py
+++ jarviscli/plugins/calculator.py
@@ -43,38 +43,38 @@ class Calculator:
         self.plusbtn.place(x=8, y=165)
 
         self.minusbtn = tk.Button(
-                       self.calcwin, text="-", font=("Elephant", 20),
-                       command=self.minus)
+            self.calcwin, text="-", font=("Elephant", 20),
+            command=self.minus)
         self.minusbtn.config(width=3)
         self.minusbtn.place(x=93, y=165)
 
         self.multbtn = tk.Button(
-                      self.calcwin, text="x", font=("Elephant", 20),
-                      command=self.mult)
+            self.calcwin, text="x", font=("Elephant", 20),
+            command=self.mult)
         self.multbtn.config(width=3)
         self.multbtn.place(x=178, y=165)
 
         self.divbtn = tk.Button(
-                     self.calcwin, text="/", font=("Elephant", 20),
-                     command=self.div)
+            self.calcwin, text="/", font=("Elephant", 20),
+            command=self.div)
         self.divbtn.config(width=3)
         self.divbtn.place(x=263, y=165)
 
         self.sqrootbtn = tk.Button(
-                        self.calcwin, text="sqrt", font=("Elephant", 20),
-                        command=self.sqroot)
+            self.calcwin, text="sqrt", font=("Elephant", 20),
+            command=self.sqroot)
         self.sqrootbtn.config(height=1, width=3)
         self.sqrootbtn.place(x=8, y=245)
 
         self.factbtn = tk.Button(
-                      self.calcwin, text="!", font=("Elephant", 20),
-                      command=self.fact)
+            self.calcwin, text="!", font=("Elephant", 20),
+            command=self.fact)
         self.factbtn.config(width=3)
         self.factbtn.place(x=93, y=245)
 
         self.powerbtn = tk.Button(
-                       self.calcwin, text="x^y", font=("Elephant", 20),
-                       command=self.power)
+            self.calcwin, text="x^y", font=("Elephant", 20),
+            command=self.power)
         self.powerbtn.config(height=1, width=3)
         self.powerbtn.place(x=178, y=245)
 
@@ -85,32 +85,32 @@ class Calculator:
         self.c_btn.place(x=263, y=245)
 
         self.sinbtn = tk.Button(
-                     self.calcwin, text="sin", font=("Elephant", 20),
-                     command=self.sin)
+            self.calcwin, text="sin", font=("Elephant", 20),
+            command=self.sin)
         self.sinbtn.config(height=1, width=3)
         self.sinbtn.place(x=8, y=325)
 
         self.cosbtn = tk.Button(
-                     self.calcwin, text="cos", font=("Elephant", 20),
-                     command=self.cos)
+            self.calcwin, text="cos", font=("Elephant", 20),
+            command=self.cos)
         self.cosbtn.config(height=1, width=3)
         self.cosbtn.place(x=93, y=325)
 
         self.tanbtn = tk.Button(
-                     self.calcwin, text="tan", font=("Elephant", 20),
-                     command=self.tan)
+            self.calcwin, text="tan", font=("Elephant", 20),
+            command=self.tan)
         self.tanbtn.config(height=1, width=3)
         self.tanbtn.place(x=178, y=325)
 
         self.equalbtn = tk.Button(
-                       self.calcwin, text="=", font=("Elephant", 20),
-                       command=lambda: self.equal(0))
+            self.calcwin, text="=", font=("Elephant", 20),
+            command=lambda: self.equal(0))
         self.equalbtn.config(width=3)
         self.equalbtn.place(x=263, y=325)
 
         self.exitbtn = tk.Button(
-                      self.calcwin, text="EXIT", bg="red", fg="white",
-                      command=lambda: self.exit(0))
+            self.calcwin, text="EXIT", bg="red", fg="white",
+            command=lambda: self.exit(0))
         self.exitbtn.place(x=150, y=405)
 
         self.keybinds()
