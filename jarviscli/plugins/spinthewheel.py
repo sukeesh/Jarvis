e4d22c66e0b4322f3347fd8f62594aaf1aceea4e
ADARSH C <chekodu.adarsh@gmail.com>
2019-11-25 21:22:47 +0530

    Spin Wheel
    Added Feature of Spin Wheel for the user to pic one random input

---
+++ jarviscli/plugins/spinthewheel.py
@@ -0,0 +1,20 @@
+from plugin import plugin
+import random
+ '''
+This code pics one of the random inputs given by the user like spin wheel
+
+ '''
+@alias("wheel spin")
+@plugin("spin wheel")
+def spin(jarvis, s):
+	jarvis.say(' ')
+	jarvis.say('welcome to spin the wheel\n')
+	jarvis.say('enter the number of elements in the wheel')
+	num = jarvis.input()
+	jarvis.say('enter the elements one after another\n')
+	wheel = []
+	for i in range(0,num):
+		entry = jarvis.input()
+		wheel.append(entry)
+	jarvis.say('Let the wheel spin !!!!!!!!!\n')
+	jarvis.say('result is: ' + random.choice(wheel))
