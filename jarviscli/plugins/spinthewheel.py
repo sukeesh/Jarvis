bc24297b0a1472b6f3e45609fea9955c2f987374
ADARSH C <chekodu.adarsh@gmail.com>
2019-11-26 22:24:27 +0530

    PEP8 standards updated

--- jarviscli/plugins/spinthewheel.py
+++ jarviscli/plugins/spinthewheel.py
@@ -1,18 +1,19 @@
 from plugin import plugin
 import random
-'''This code picks one of the random inputs given by the user like spin wheel'''
+'''This code picks one of the random inputs given by the user
+like spin wheel'''
 
 
 @plugin("spin wheel")
 def spin(jarvis, s):
-	jarvis.say(' ')
-	jarvis.say('welcome to spin the wheel\n')
-	jarvis.say('enter the number of elements in the wheel')
-	num = jarvis.input()
-	jarvis.say('enter the elements one after another\n')
-	wheel = []
-	for i in range(0,num):
-		entry = jarvis.input()
-		wheel.append(entry)
-	jarvis.say('Let the wheel spin !!!!!!!!!\n')
-	jarvis.say('result is: ' + random.choice(wheel))
+    jarvis.say(' ')
+    jarvis.say('welcome to spin the wheel\n')
+    jarvis.say('enter the number of elements in the wheel')
+    num = jarvis.input()
+    jarvis.say('enter the elements one after another\n')
+    wheel = []
+    for i in range(0, num):
+        entry = jarvis.input()
+        wheel.append(entry)
+    jarvis.say('Let the wheel spin !!!!!!!!!\n')
+    jarvis.say('result is: ' + random.choice(wheel))
