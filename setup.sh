58bba1994f84ef0bd73de2b725e5e2ef49bf8ef3
pnhofmann <foss@pnhofmann.de>
2019-11-27 12:18:21 +0100

    Setup.sh: check python, python2 or python3 available

--- setup.sh
+++ setup.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 cd $(dirname $0)
-python installer
+
+if python --version; then
+	python installer
+elif python3 --version; then
+	python3 installer
+elif python2 --version; then
+	python2 installer
+else
+	echo "Could not find Python installation"
+fi
