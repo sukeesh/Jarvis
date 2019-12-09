c1f79f11def12dbadcac619320377cad3ae070ad
baptoutiego <bgautier94100@gmail.com>
2019-12-09 19:10:02 +0100

    integrate plugin and add pdfkit in requirements

--- jarviscli/plugins/htmltopdf.py
+++ jarviscli/plugins/htmltopdf.py
@@ -2,14 +2,20 @@ import pdfkit
 from plugin import plugin
 
 
-
-
-
-@require(platform=[LINUX], python=PYTHON3, network=True, native=["wkhtmltopdf"])
 @plugin("htmltopdf")
 def htmltopdf(jarvis, s):
-    """transform your url page into a pdf file in the source directory of jarvis type your url as the following : \n
-    'google.com' """
+    
+    """transform your url page into a pdf file in the Jarvis source directory. type your url as the following : \n
+    'htmltopdf google.com'\n
+    The output file will be the following : \n
+    'google.com.pdf'"""
 
-    splitUrl = s.split('.')
-    pdfkit.from_url(s, splitUrl[0] + '.pdf')
+    dots = ["com", "org", "fr", "en"]
+    for el in dots:
+    	if el in s:
+    		s = s.replace(el,"."+el)
+
+    if not s:
+    	jarvis.say("please enter an url after calling the plugin")
+    else:
+    	pdfkit.from_url(s, s + '.pdf')
