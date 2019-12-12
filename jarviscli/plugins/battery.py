69842631d4debce27a2a3bea1c298ab34bd43f28
pnhofmann <foss@pnhofmann.de>
2019-12-12 15:20:55 +0100

    Battery (Linux): Check if / what battery exist

--- jarviscli/plugins/battery.py
+++ jarviscli/plugins/battery.py
@@ -107,8 +107,16 @@ def battery_LINUX_FALLBACK(jarvis, s):
 
     # Get the battery info
     # https://askubuntu.com/a/309146
-    battery_dir = "/sys/class/power_supply/BAT0/"
+    battery_dir = False
+    for bat_num in range(10):
+        battery_dir_check = "/sys/class/power_supply/BAT{}/".format(str(bat_num))
+        if os.path.exists(battery_dir_check):
+            battery_dir = battery_dir_check
+            break
 
+    if battery_dir is False:
+        jarvis.say("No Battery found!")
+        return
 
     def get_battery_info(info):
         return subprocess.check_output(["cat", battery_dir + info]).decode("utf-8")[:-1]
