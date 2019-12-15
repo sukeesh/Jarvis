3dc4cea4c7a93c028a173683c0bd688fef2773b1
Baptiste Gautier <33863595+baptoutiego@users.noreply.github.com>
2019-12-15 11:41:33 +0100

    correct syntax error

--- jarviscli/plugins/htmltopdf_url.py
+++ jarviscli/plugins/htmltopdf_url.py
@@ -1,7 +1,6 @@
 import pdfkit
 from plugin import plugin, require, LINUX
 
-
 @require(platform=LINUX, native=["wkhtmltopdf"], network=True)
 @plugin("htmltopdf url")
 class htmltopdf_url:
@@ -12,18 +11,17 @@ class htmltopdf_url:
     'google.com.pdf'
     """
     def __call__(self, jarvis, s):
-    	#We have to add the '.' back because the Jarvis API removes it
+        # We have to add the '.' back because the Jarvis API removes it
         dots = ["com", "org", "fr", "en"]
-	for el in dots:
+        for el in dots:
             if el in s:
-	        s = s.replace(el,"."+el)
+                s = s.replace(el, "." + el)
         if not s:
             jarvis.say("please enter an url after calling the plugin")
-        elif not '.' in s:
+        elif '.' not in s:
             jarvis.say("please make sur your url is valid")
         else:
        	    try:
                 pdfkit.from_url(s, s + '.pdf')
             except IOError as err:
-                jarvis.say("IO error: {0}".format(err) + "\nMake sure your URL is valid and that \ 
-                you have access to the internet")
+                jarvis.say("IO error: {0}".format(err) + "\nMake sure your URL is valid and that you have access to the internet")
