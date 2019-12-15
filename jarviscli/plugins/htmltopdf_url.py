90245e96bad22bf84751a87cae919255e1c2a369
Baptiste Gautier <33863595+baptoutiego@users.noreply.github.com>
2019-12-15 11:21:53 +0100

    fix pep8 for htmltopdf_url

--- jarviscli/plugins/htmltopdf_url.py
+++ jarviscli/plugins/htmltopdf_url.py
@@ -5,26 +5,25 @@ from plugin import plugin, require, LINUX
 @require(platform=LINUX, native=["wkhtmltopdf"], network=True)
 @plugin("htmltopdf url")
 class htmltopdf_url:
-    
-    """transform your url page into a pdf file in the Jarvis source directory. type your url as the following:
+    """
+    Transform your url page into a pdf file in the Jarvis source directory. type your url as the following:
     'htmltopdf google.com'
     The output file will be the following:
-    'google.com.pdf'"""
-
+    'google.com.pdf'
+    """
     def __call__(self, jarvis, s):
-    	
-    	#We have to add the '.' back beacause the Jarvis API removes it
-	    dots = ["com", "org", "fr", "en"]
-	    for el in dots:
-	        if el in s:
-	            s = s.replace(el,"."+el)
-
-	    if not s:
-	        jarvis.say("please enter an url after calling the plugin")
-	    elif not '.' in s:
-	    	jarvis.say("please make sur your url is valid")
-	    else:
-	    	try:
-	        	pdfkit.from_url(s, s + '.pdf')
-	    	except IOError as err:
-	    		jarvis.say("IO error: {0}".format(err) + "\nMake sure your URL is valid and that you have access to the internet")
+    	#We have to add the '.' back because the Jarvis API removes it
+        dots = ["com", "org", "fr", "en"]
+	for el in dots:
+            if el in s:
+	        s = s.replace(el,"."+el)
+        if not s:
+            jarvis.say("please enter an url after calling the plugin")
+        elif not '.' in s:
+            jarvis.say("please make sur your url is valid")
+        else:
+       	    try:
+                pdfkit.from_url(s, s + '.pdf')
+            except IOError as err:
+                jarvis.say("IO error: {0}".format(err) + "\nMake sure your URL is valid and that \ 
+                you have access to the internet")
