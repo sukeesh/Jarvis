8e6943b97f9b580c7fde61e8af3787aba1744aa5
Baptiste Gautier <33863595+baptoutiego@users.noreply.github.com>
2019-12-15 11:03:46 +0100

    fix pep8 for htmltotpdf

--- jarviscli/plugins/htmltopdf.py
+++ jarviscli/plugins/htmltopdf.py
@@ -5,14 +5,6 @@ from plugin import plugin, require, LINUX
 @require(platform=LINUX, native=["wkhtmltopdf"])
 @plugin("htmltopdf")
 class htmltopdf:
-
     """Convert your html file or web page into pdf file"""
-
     def __call__(self, jarvis, s):
         jarvis.say("Welcome to the htmltopdf convertor! \nType 'help htmltopdf' to learn how to use it")
-
-
-
-
-
-
