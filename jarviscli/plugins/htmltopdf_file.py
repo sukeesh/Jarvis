409701f1597a0846ab51df4a34bb17639131645d
Baptiste Gautier <33863595+baptoutiego@users.noreply.github.com>
2019-12-15 11:14:18 +0100

    correct text error

--- jarviscli/plugins/htmltopdf_file.py
+++ jarviscli/plugins/htmltopdf_file.py
@@ -5,19 +5,21 @@ from plugin import plugin, require,  LINUX
 @require(platform=LINUX, native=["wkhtmltopdf"])
 @plugin("htmltopdf file")
 class htmltopdf_file:
-    """transform your html file into a pdf file in the Jarvis source directory.
+    """
+    Transform your html file into a pdf file in the Jarvis source directory.
     Type your url as the following:
     'htmltopdf example.html'
     The output file will be the following:
     'example.pdf'
-    Your html file must be in the jarvis source directory"""
+    Your html file must be in the jarvis source directory
+    """
     def __call__(self, jarvis, s):
         if not s:
             jarvis.say("please enter a file name after calling the plugin")
         elif not "html" in s:
             jarvis.say("Your file must end with '.html'")
         else:
-            #We have to add the '.' back beacause the Jarvis API removes it
+            #We have to add the '.' back because the Jarvis API removes it
             s = s.replace('html', '.' + 'html')
             try:
                 pdfkit.from_file(s, s.replace('.html', '') + '.pdf')
