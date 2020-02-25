150fb7db35b274cb705fd463c3fd066d55c9b9a3
ADARSH C <chekodu.adarsh@gmail.com>
2020-02-25 19:42:06 +0530

    Solved bugs in bulkresize.py

--- jarviscli/plugins/bulkresize.py
+++ jarviscli/plugins/bulkresize.py
@@ -9,15 +9,21 @@ def bulk_resizer(input_path, output_path, desired_size=32,
                  color=[0, 0, 0], rename=True):
     img_no = 0
 
-    onlyfiles = os.listdir(input_path)
-    # print(onlyfiles)
-    for im_pth in onlyfiles:
+    filename = os.listdir(input_path)
+    filepath = []
+    for name in filename:
+        path = input_path+"/"+name
+        if os.path.isfile(path):
+            filepath.append(path)
+            print(path)
+    for im_pth in filepath:
         try:
             im = cv2.imread(im_pth)
             old_size = im.shape[:2]
+
         except:
             continue
-
+        print("impath: ",im_pth)
         ratio = float(desired_size) / max(old_size)
         new_size = tuple([int(x * ratio) for x in old_size])
 
@@ -30,15 +36,14 @@ def bulk_resizer(input_path, output_path, desired_size=32,
         top, bottom = delta_h // 2, delta_h - (delta_h // 2)
         left, right = delta_w // 2, delta_w - (delta_w // 2)
 
-        new_im = cv2.copyMakeBorder(im, top, bottom, left, right,
-                                    cv2.BORDER_CONSTANT, value=color)
+        new_im = cv2.copyMakeBorder(im, top, bottom, left, right,cv2.BORDER_CONSTANT, value=color)
         if rename:
             output_path1 = output_path + "/" + str(img_no) + ".jpg"
             img_no += 1
         else:
             output_path1 = output_path + "/" + \
                 os.path.splitext(os.path.basename(im_pth))[0] + ".jpg"
-
+        print(output_path1)
         cv2.imwrite(output_path1, new_im)
 
 
@@ -54,15 +59,15 @@ def spin(jarvis, s):
     jarvis.say('This is bulk resizer it will resize the image into given',
                'size with padding !!!! Specially designed for Deep Leanring',
                ' data collection process')
-    jarvis.say('Enter the path of directory with images to be resized :')
+    jarvis.say('Enter the path of directory with images to be resized : ')
     path1 = jarvis.input()
     jarvis.say('Should I rename them to non repeating whole number series?',
-               ' y/n :')
+               'y/n :')
     rename = jarvis.input()
     jarvis.say('Enter the path of output directory :')
     path2 = jarvis.input()
     jarvis.say("Enter the target size :")
-    size = jarvis.input_number()
+    size = jarvis.input_number(rtype=int)
     if rename == 'y':
         bulk_resizer(path1, path2, size, [0, 0, 0], True)
     else:
