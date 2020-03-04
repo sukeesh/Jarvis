b12351c032a735a33aa3fcdfe68bdb9eb14bed58
DannKim <denis.kim@nu.edu.kz>
2020-03-04 13:46:29 +0600

    edit workout

--- jarviscli/plugins/workout.py
+++ jarviscli/plugins/workout.py
@@ -1,6 +1,44 @@
 from plugin import plugin
 from colorama import Fore
 
+
+def pushups(jarvis, s):
+	try:
+		maximum = int(s);
+	except:
+		jarvis.say("Please enter an integer only!", Fore.BLUE)
+		quit(jarvis)
+		return
+	num = maximum*2//5
+	rest = 30
+	jarvis.say("Your program for today is 5 sets of " + str(num) + " times with rests of " + str(rest) + "sec", Fore.BLUE);
+	s = jarvis.input("Type 's' to start and 'q' for quit\n", Fore.GREEN)
+	if (s == "'q'" or s == "q"):
+		quit(jarvis)
+	elif (s == "'s'" or s == "s"):
+		for i in range(1,6):
+			jarvis.say("Set "+ str(i) + " Do " + str(num) + " times", Fore.BLUE)
+			jarvis.input("Press enter after finishing", Fore.GREEN)
+		jarvis.say("Well done, you did " + str(num*5) + " times pushups", Fore.BLUE)
+		quit(jarvis)
+	else:
+		jarvis.say("Incorrect input, please write either 'push' or 'pull'", Fore.BLUE)
+		quit(jarvis)
+
+def pullups(jarvis, s):
+	try:
+		int(s);
+	except:
+		jarvis.say("Please enter an integer only!", Fore.BLUE)
+		quit(jarvis)
+		return
+	jarvis.say("do " + s + " times pullups");
+
+
+def quit(jarvis):
+	jarvis.say("Stay fit - do workout!", Fore.BLUE)
+
+
 @plugin("workout")
 def workout(jarvis, s):
     """Provides a workout programm according to user's abilities"""
@@ -16,27 +54,3 @@ def workout(jarvis, s):
     else:
     	jarvis.say("Incorrect input, please write either 'push' or 'pull'", Fore.BLUE)
     	quit(jarvis)
-
-
-def pushups(jarvis, s):
-	try:
-		int(s);
-	except:
-		jarvis.say("Please enter an integer only!", Fore.BLUE)
-		quit(jarvis)
-		return
-	jarvis.say("do " + s + " times pushups");
-
-
-def pullups(jarvis, s):
-	try:
-		int(s);
-	except:
-		jarvis.say("Please enter an integer only!", Fore.BLUE)
-		quit(jarvis)
-		return
-	jarvis.say("do " + s + " times pullups");
-
-
-def quit(jarvis):
-	jarvis.say("Stay fit - do workout!", Fore.GREEN)
