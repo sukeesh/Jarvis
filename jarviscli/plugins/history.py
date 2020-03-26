a46d8199014d3644e2f145174f2f4c2fb1ef176d
CSenshi <saba.pochkhua@gmail.com>
2020-03-27 02:47:48 +0400

    implement configuration parsing (for API usage)

--- jarviscli/plugins/history.py
+++ jarviscli/plugins/history.py
@@ -1,4 +1,5 @@
 from plugin import plugin, require
+import random
 
 @require(network=True)
 @plugin('history')
@@ -11,22 +12,52 @@ class history:
     """
 
     def __init__(self):
-        self.url = "http://history.muffinlabs.com/date/{}/{}" # .format(month, day)
+        self.url = "http://history.muffinlabs.com/date"
         self.events = {'birth', 'death', 'events'}
-        self.months = {'january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'}
+        self.months = {'january', 'february', 'march', 'april', 'may', 'june',
+                       'july', 'august', 'september', 'october', 'november', 'december'}
+        self.keywords = {'today'}
 
     def __call__(self, jarvis, s):
         config = self._parse_arguments(s)
-        print(config)
-        
+        api_args = self._parse_config(config)
+        print(api_args)
+
     def _parse_arguments(self, args):
         split_args = args.split()
-        cfg = {}
+        cfg = {'event': None, 'month': None, 'day': None, 'keywords': set()}
         for arg in split_args:
-            if arg in self.events:
+            if arg.isdigit():
+                cfg['day'] = arg
+            elif arg in self.events:
                 cfg['event'] = arg
-            elif arg  in self.months:
+            elif arg in self.months:
                 cfg['month'] = arg
-            elif arg.isdigit():
-                cfg['day']  = arg
+            elif arg in self.keywords:
+                cfg['keywords'].add(arg)
         return cfg
+
+    def _parse_config(self, config):
+        api_cfg = {}
+
+        # check for today keyword
+        api_cfg['today'] = False
+        if 'today' in config['keywords']:
+            api_cfg['today'] = True
+
+        # check for events
+        api_cfg['event'] = config['event']
+        if not api_cfg['event']:
+            api_cfg['event'] = random.choice(list(self.events))
+
+        # check for month
+        api_cfg['month'] = config['month']
+        if not api_cfg['month']:
+            api_cfg['month'] = random.choice(list(self.months))
+
+        # check for day
+        api_cfg['day'] = config['day']
+        if not api_cfg['day']:
+            api_cfg['day'] = random.randint(1, 29)
+
+        return api_cfg
