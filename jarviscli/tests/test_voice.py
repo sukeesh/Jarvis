cbd4d7186dec0845d2fd56103fac7833bfbb689b
Methenitis <giannis.methen@gmail.com>
2020-05-11 14:05:32 +0300

    Fixing the voice unit test

--- jarviscli/tests/test_voice.py
+++ jarviscli/tests/test_voice.py
@@ -2,35 +2,35 @@ import unittest
 from tests import PluginTest
 from plugins import voice
 from CmdInterpreter import JarvisAPI
+from Jarvis import Jarvis
 
 # this test class contains test cases for the plugins "gtts" and "disable_gtts"
 # which are included in the "voice.py" file in the "plugins" folder
 
-
 class VoiceTest(PluginTest):
 
     # test "gtts" plugin
-    def setUp1(self):
+    def setUp(self):
         self.test = self.load_plugin(voice.gtts)
 
     def test_gtts(self):
         # run "gtts" plugin code
-        self.test.run = self.load_plugin(voice.gtts)
+        self.test.run(voice.gtts)
 
         # verify that "gtts" plugin code works
         self.assertEqual(self._jarvis.gtts_status, True)
 
+
     # test "disable_gtts" plugin
-    def setUp2(self):
+    def setUp(self):
         self.test = self.load_plugin(voice.disable_gtts)
 
     def test_disable_gtts(self):
         # run "disable_gtts" plugin code
-        self.test.run = self.load_plugin(voice.disable_gtts)
+        self.test.run(voice.disable_gtts)
 
         # verify that "disable_gtts" plugin code works
         self.assertEqual(self._jarvis.gtts_status, False)
-
 
 if __name__ == '__main__':
     unittest.main()
