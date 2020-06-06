eded1b4403c34fde4598b6b50e0909334c2eddee
Darin755 <darinrawson@gmail.com>
2020-06-06 13:18:12 -0600

    update open.py to PEP8

--- jarviscli/plugins/open.py
+++ jarviscli/plugins/open.py
@@ -1,12 +1,14 @@
 import os
 from plugin import plugin, require, LINUX
-@require(platform=LINUX)
 
+
+@require(platform=LINUX)
 @plugin('open')
-def open(jarvis,s):
-	if(s != ""):
-		string = "xdg-open " + "/usr/share/applications/" + s + ".desktop"
-		os.system(string)
-	else:
-		jarvis.say("avaliable options are:")
-		os.system("ls -1 /usr/share/applications/ | sed -e 's/\.desktop$//'")
+def open(jarvis, s):
+
+    if(s != ""):
+        string = "xdg-open " + "/usr/share/applications/" + s + ".desktop"
+        os.system(string)
+    else:
+        jarvis.say("avaliable options are:")
+        os.system("ls -1 /usr/share/applications/ | sed -e 's/\.desktop$//'")
