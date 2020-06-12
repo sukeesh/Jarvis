48e35f8a26f7782c3470787ccf954b6c468fb5b7
Anna Kotsa <44175203+annakotsa@users.noreply.github.com>
2020-06-12 21:29:55 +0300

    Add Calories feature to Jarvis Alexa Skill
    Co-authored by Giannis Methenitis (@methenitis)

--- Jarvis Alexa Skill/Jarvis Alexa Skill.py
+++ Jarvis Alexa Skill/Jarvis Alexa Skill.py
@@ -13,6 +13,11 @@ logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 def spinit(list):
     return(random.choice(list))
 
+def exercise_level(self, level):
+    multipliers = {1: 1.2, 2: 1.4, 3: 1.6, 4: 1.95}
+    multiplier = multipliers.get(level, 1)
+    return multiplier
+
 @ask.intent("AnswerIntent")
 def answer(ans):
 
@@ -37,7 +42,28 @@ def answer(ans):
             res = spinit(wheel)
             msg = render_template('result')
             return statement(msg)
+        
+    elif ans == "Calories" :
+        def enter_cal():
+            ent_msg = render_template('enter_cal')
+            return statement(ent_msg)
 
+        @ask.intent("CaloriesAnswerIntent", convert={'age': int, 'height': int, 'weight': float, 'level': int})
+        def answer(age, height, weight, level, gender):
+            gender_no = 0
+            if gender == "male" or gender == "man" or gender == "m" :
+                gender_no = 5
+            elif gender == "female" or gender == 'woman' or gender == "f" :
+                gender_no = -161
+            brm = float(10 * weight + 6.25 * height - 5
+                * age + gender_no) * self.exercise_level(level)
+            brm_loss = brm - 500.0
+            brm_put_on = brm + 500.0
+            dci = str(brm)
+            lwc = str(brm_loss)
+            powc = str(brm_put_on)
+            msg = render_template('cal_result')
+            return statement(msg)
 @ask.launch
 def new_game():
     welcome_msg = render_template('welcome_jarvis')
