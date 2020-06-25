478baa9724bf30152d8414f2dbf72b6274980411
Jay Rajput <jay24rajput@gmail.com>
2020-06-26 01:50:00 +0530

    Added Support for multiple images in the same folder - The images in a folder will be converted to a pdf in the same directory as the folder.

--- jarviscli/plugins/imgtopdf.py
+++ jarviscli/plugins/imgtopdf.py
@@ -1,7 +1,7 @@
 from fpdf import FPDF
-import img2pdf 
+import img2pdf
 from PIL import Image
-import os 
+import os
 from plugin import plugin
 from colorama import Fore
 
@@ -13,10 +13,9 @@ class ImageToPDF:
     """
 
     def __init__(self):
-        #Path of the folder or image to be converted
+        # Path of the folder or image to be converted
         self.path = None
         self.image = None
-        self.pdf = None
 
     def __call__(self, jarvis, s):
         self.imgtopdf(jarvis)
@@ -34,27 +33,34 @@ class ImageToPDF:
                 jarvis.say("See you next time :D", Fore.CYAN)
                 break
 
+            # For single image to be converted to pdf
             elif user_input == '1':
                 image_path = jarvis.input('Enter the full path of the image: ')
                 pdf_bytes = self.single_image_to_pdf(jarvis, image_path)
 
+            # For multiple images in a folder to be converted to pdf
             elif user_input == '2':
-                self.folder_to_pdf(jarvis)
+                folder_path = jarvis.input(
+                    'Enter the full path of the folder: ')
+                pdf_bytes = self.folder_to_pdf(jarvis, folder_path)
 
+            # For an incorrectly entered option
             else:
                 self.incorrect_option(jarvis)
-                # For skipping the code written after else
                 continue
 
-            user_choice = jarvis.input('Would you like to save the file in the same folder?[y/n] ')
+            # Choosing the pdf file saving destination
+            user_choice = jarvis.input(
+                'Would you like to save the file in the same folder?[y/n] ')
             user_choice = user_choice.lower()
             if user_choice == 'yes' or user_choice == 'y':
                 destination = self.getParentDirectory(self.path)
             elif user_choice == 'no' or user_choice == 'n':
                 destination = jarvis.input('Enter the folder destination: ')
 
+            # Naming and saving the pdf file
             file_name = jarvis.input('What would you like to name your pdf? ')
-            destination = destination + '/'+ file_name + '.pdf'
+            destination = destination + '/' + file_name + '.pdf'
             print('Final Destination ' + destination)
             self.save_pdf(jarvis, pdf_bytes, destination)
 
@@ -70,24 +76,32 @@ class ImageToPDF:
 
     def single_image_to_pdf(self, jarvis, image_path):
         """
-        This function is used to convert a single image 
+        This function is used to convert a single image
         with a given path to a pdf file.
         """
         self.path = image_path
-        self.image = Image.open(image_path) 
+        self.image = Image.open(image_path)
         pdf_bytes = img2pdf.convert(self.image.filename)
+        self.image.close()
         return pdf_bytes
 
-    def folder_to_pdf(self, jarvis):
+    def folder_to_pdf(self, jarvis, folder_path):
         """
         This function is used to convert all the images
         in a given folder path to a single PDF file
         """
-        pass
+        self.path = folder_path
+        source_images = []
+        os.chdir(self.path)
+        for image in os.listdir(os.getcwd()):
+            if image.endswith('.jpg') or image.endswith('.png'):
+                source_images.append(image)
+        pdf_bytes = img2pdf.convert(source_images)
+        return pdf_bytes
 
     def incorrect_option(self, jarvis):
         """
-        A function to notify the user that an incorrect option 
+        A function to notify the user that an incorrect option
         has been entered and prompting him to enter a correct one
         """
         jarvis.say("Oops! Looks like you entered an incorrect option", Fore.RED)
@@ -100,14 +114,13 @@ class ImageToPDF:
         """
         pdf_file = open(destination, 'wb')
         pdf_file.write(pdf_bytes)
-        self.image.close()
         pdf_file.close()
         jarvis.say('Your pdf is created successfully', Fore.GREEN)
 
     def getParentDirectory(self, path):
         """
-        Removes the image name from the folder and returns 
-        the remaining path. 
+        Removes the image name from the folder and returns
+        the remaining path
         """
         path = path.split('/')
         path.pop()
