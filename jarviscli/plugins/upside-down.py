4ba3a22899bdd916349588e363efd77cb1bc1aee
pnhofmann <foss.github@pnhofmann.de>
2020-07-13 10:07:03 +0000

    PEP8: Clean up empty lines

--- jarviscli/plugins/upside-down.py
+++ jarviscli/plugins/upside-down.py
@@ -2,15 +2,11 @@ from plugin import plugin
 from colorama import Fore
 
 
-
 @plugin('upside down')
 def generate_random_list(jarvis, str):
     user_input = jarvis.input("Enter string to be converted to upside-down (only english letters will be converted): ")
     result = convert_input(jarvis, user_input)
     jarvis.say(result, Fore.GREEN)
-
-
-
 
 
 def convert_input(jarvis, u_input):
@@ -23,7 +19,3 @@ def convert_input(jarvis, u_input):
         if letter in converter_dict:
             result += converter_dict[letter]
     return result[::-1]
-
-
-
-
