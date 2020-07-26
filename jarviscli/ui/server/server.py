8e426611ce759bb97783c3ed4743a17bd065b699
pnhofmann <foss@pnhofmann.de>
2020-07-26 12:23:15 +0200

    Implement server plugin entry point

--- jarviscli/ui/server/server.py
+++ jarviscli/ui/server/server.py
@@ -1,12 +1,39 @@
-from flask import Flask
 import re
+
+from flask import Flask
+
+from jarvis import Jarvis
+from language import snips
+
+
+class ServerIO:
+    def __init__(self):
+        self.recorded_texts = []
+
+    def say(self, text, color=''):
+        self.recorded_texts.append(text)
+
+    def input(self, prompt="", color=""):
+        # TO IMPLEMENT PROBABLY LATER
+        # because 'input' might be tricky to implement
+        pass
+
+    def exit(self):
+        # STOP SERVER
+        pass
+
+    def fetch_recorded_texts(self):
+        tmp = self.recorded_texts
+        self.recorded_texts = []
+        return tmp
 
 
 class JarvisServer:
 
     def __init__(self):
+
         self.server_app = Flask(__name__)
-        self.host_name = "192.168.1.234"
+        self.host_name = "0.0.0.0"
         self.port = 8008
         self.auth_string = "new String that I created just so I could access this just for myself"
         self.routes = [
@@ -15,6 +42,16 @@ class JarvisServer:
 
     def start_server(self):
         print("Starting a thread for home server!")
+
+        from main import build_plugin_manager
+        plugin_manager = build_plugin_manager()
+        language_parser = snips.LanguageParser()
+        jarvis_server = JarvisServer()
+        self.io = ServerIO()
+
+        self.jarvis = Jarvis(language_parser, plugin_manager, jarvis_server)
+        self.jarvis.register_io(self.io)
+
         self.server_app.run(host=self.host_name, port=self.port, threaded=True, debug=True, use_reloader=False)
 
     def check_running(self) -> bool:
@@ -25,11 +62,22 @@ class JarvisServer:
         self._get_all_routes(jarvis_plugins)
 
         for route in self.routes:
-            self.server_app.add_url_rule(
-                route["route"],
-                route["endpoint"],
-                route["func"]
-            )
+            try:
+                self.server_app.add_url_rule(
+                    route["route"],
+                    route["endpoint"],
+                    route["func"]
+                )
+            except Exception as e:
+                print(e)
+
+    def _wrap_plugin(self, plugin):
+        def _run():
+            plugin.run(self.jarvis.jarvis_api, '')
+            _text = self.io.fetch_recorded_texts()
+            print(_text)
+            return '\n'.join(_text)
+        return _run
 
     def _get_all_routes(self, jarvis_plugins):
         for plugin in jarvis_plugins:
@@ -41,7 +89,8 @@ class JarvisServer:
             else:
                 route["route"] = endpoint_string
                 route["endpoint"] = endpoint_string
-                route["func"] = plugin
+                _plug = plugin
+                route["func"] = self._wrap_plugin(plugin)
                 self.routes.append(route)
 
     def _health(self):
