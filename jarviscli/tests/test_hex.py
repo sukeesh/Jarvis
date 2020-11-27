a7bfcaba0477de44e1efeacf2eb76d072ac1aab8
Ian Zhang <ianzhang0510@gmail.com>
2020-11-26 19:01:33 -0500

    Added plugin to convert an integer into a hexadecimal number

---
+++ jarviscli/tests/test_hex.py
@@ -0,0 +1,28 @@
+import unittest
+from tests import PluginTest
+from plugins import hex
+
+
+class HexTest(PluginTest):
+    def setUp(self):
+        self.test = self.load_plugin(hex.hex)
+
+    def test_0(self):
+        self.test.run("0")
+        self.assertEqual(self.history_say().last_text(), "0")
+
+    def test_2(self):
+        self.test.run("2")
+        self.assertEqual(self.history_say().last_text(), "2")
+
+    def test_4932(self):
+        self.test.run("4932")
+        self.assertEqual(self.history_say().last_text(), "1344")
+
+    def test_negative_1205(self):
+        self.test.run("-1205")
+        self.assertEqual(self.history_say().last_text(), "-4B5")
+
+
+if __name__ == '__main__':
+    unittest.main()
