6eb030e480091edea790465ffab7b9b7fdcc009e
Metodi <metodi.istatkov@gmail.com>
2021-01-21 15:34:46 +0200

    first test for scan_network

---
+++ jarviscli/tests/test_scan_network.py
@@ -0,0 +1,19 @@
+import unittest
+from tests import PluginTest
+from plugins.scan_network import scan
+
+
+class ScanNetworkTest(PluginTest):
+    def setUp(self):
+        self.test = self.load_plugin(scan)
+
+    def test_TESTCASE_1(self):
+        # run code
+        self.test.run('127.0.0.1')
+
+        # verify that code works
+        self.assertIn('127.0.0.1', self.history_say().view_text())
+
+
+if __name__ == '__main__':
+    unittest.main()
