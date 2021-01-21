f7587443df9b9b5771cbc8892248dac0ad888497
pnhofmann <foss@pnhofmann.de>
2021-01-21 11:45:27 +0100

    Market Place: Load plugins in own directory

--- jarviscli/plugins/market_find.py
+++ jarviscli/plugins/market_find.py
@@ -2,21 +2,22 @@
 # Webscrapping
 import requests
 from bs4 import BeautifulSoup
-# Jarvis 
+# Jarvis
 from colorama import Fore
 from plugin import plugin, require
+
 
 @require(network=True)
 @plugin('market find')
 def market_search(jarvis, s):
     """Search Github Topics (a.k.a. market) to find baskets
     (single or bunch of plugins together).
-    
+
     Check the PLUGIN_MARKETPLACE.md for more information.
-    
+
     Args:
     * n:
-    You can use the "n=" or "n =" argument to delimit the number of 
+    You can use the "n=" or "n =" argument to delimit the number of
     repository shown. Ex: "market find n=15"
     """
     # Downloading page
@@ -29,7 +30,7 @@ def market_search(jarvis, s):
     # Finding all the links
     links_repos_objs = soup.find_all('a', class_='text-bold')
     repo_links = [repo_link['href'] for repo_link in links_repos_objs]
-    
+
     # Listing all baskets
     jarvis.say('Searching..')
     n_limit = parse_items_limit(s)
@@ -47,8 +48,9 @@ def market_search(jarvis, s):
         else:
             colour = Fore.GREEN
             colour_number = 1
-            
+
         jarvis.say(link, colour)
+
 
 def parse_items_limit(text, default=10):
     """If the user provides the argument 'n=...'
@@ -59,5 +61,5 @@ def parse_items_limit(text, default=10):
         return default
     else:
         text = text.replace(which_arg_used[0], '').replace(' ', '')
-    
+
     return int(text)
