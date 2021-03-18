a0646ec45248b47104d91b826722bcd8d57be332
pierre <pierre@cavapas.fr>
2021-03-18 09:06:53 +0100

    add agify feature

---
+++ jarviscli/plugins/agify.py
@@ -0,0 +1,21 @@
+import requests
+from colorama import Fore
+from plugin import plugin, require
+
+@require(network=True)
+@plugin("agify")
+def agify(jarvis, s):
+    """Tells the age of someone based on his name, powered by Pierrecoulon1"""
+
+    if (s):
+        req = requests.get("https://api.agify.io?name=" + s)
+        data = req.json()
+        if data == "":
+            jarvis.say("Sorry, an error occured", Fore.BLUE)
+            return
+        response = "I think you are " + str(data.get('age'))
+
+        jarvis.say(response, Fore.BLUE)
+    
+    else:
+        jarvis.say("Please give me the name of someone", Fore.BLUE)
