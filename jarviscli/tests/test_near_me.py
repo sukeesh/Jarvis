e427a7cd3bc960648350a13343f688f58d9a10a1
nmoatsos <56679780+NikolasMoatsos@users.noreply.github.com>
2021-05-05 15:01:12 +0300

    Update tests for near_me and add test class for directions_to

--- jarviscli/tests/test_near_me.py
+++ jarviscli/tests/test_near_me.py
@@ -3,17 +3,36 @@ from mock import patch
 from packages import near_me, mapps
 
 
-class NearMeTest(unittest.TestCase):
-
-    def setUp(self):
-        self.things = 'charities'
-        self.city = 'Valencia'
+class TestNearMe(unittest.TestCase):
+    """
+    A test class that contains test cases for the main method of
+    near_me.
+    """
 
     @patch.object(mapps, 'search_near')
-    def test_what_to_search_where_is_passed_to_mapps(self, mock_search_near):
-        data = "{} | {}".format(self.things, self.city)
+    def test_near_with_things_and_specific_city(self, mock_search_near):
+        things = 'charities'
+        city = 'Valencia'
+        data = "{} | {}".format(things, city)
         near_me.main(data)
-        mock_search_near.assert_called_once_with(self.things, self.city)
+        mock_search_near.assert_called_once_with(things, city)
+
+    @patch.object(mapps, 'search_near')
+    def test_near_with_things_and_my_location(self, mock_search_near):
+        things = 'restaurants'
+        city = 'me'
+        expected_city = 0
+        data = "{} | {}".format(things, city)
+        near_me.main(data)
+        mock_search_near.assert_called_once_with(things, expected_city)
+
+    @patch.object(mapps, 'search_near')
+    def test_near_with_things_and_empty_space(self, mock_search_near):
+        things = 'bars'
+        city = ''
+        data = "{} | {}".format(things, city)
+        near_me.main(data)
+        mock_search_near.assert_called_once_with(things, city)
 
 
 if __name__ == '__main__':
