1c3a8aa8bc5c38c72e8420b0556854ea3208c640
nikiboura <nikiboura@gmail.com>
2021-05-08 16:50:36 +0300

    Fix clear plugin for Windows

--- jarviscli/plugins/clear.py
+++ jarviscli/plugins/clear.py
@@ -1,8 +1,15 @@
-import os
+"""
+Jarvis plugin for clearing the terminal
+"""
 from plugin import plugin
+import os
 
 
-@plugin('clear')
+@plugin("clear")
 def clear(jarvis, s):
-    """Clears terminal"""
-    os.system("clear")
+    if os.name == "posix":
+        # Unix/Linux/MacOS/BSD/etc
+        os.system('clear')
+    elif os.name in ("nt", "dos", "ce"):
+        # DOS/Windows
+        os.system("cls")
