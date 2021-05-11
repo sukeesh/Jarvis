357c9a7917952b242a82e6b694e22eada72ae446
evamy <antriksh.don@gmail.com>
2021-05-11 09:45:33 -0700

    key vault inherits from Memory

--- jarviscli/packages/memory/key_vault.py
+++ jarviscli/packages/memory/key_vault.py
@@ -1,6 +1,7 @@
 import json
 import os
 from cryptography.fernet import Fernet
+from packages.memory.memory import Memory
 
 '''
 This class allows storage of values in json format. It adds an easy
@@ -25,88 +26,17 @@ example:
 module_path = os.path.dirname(__file__)
 
 
-class KeyVault:
+class KeyVault(Memory):
     """
         Initialize data with saved json file
     """
 
     def __init__(self, kv_file='key_vault.json'):
-        self.json_file = os.path.join(module_path, kv_file)
-        self.data = ''
-        # Try to open file if it doesnt exist it will throw an error
-        try:
-            with open(self.json_file, 'r') as f:
-                self.data = json.load(f)
-        except IOError:
-            # create the new file with an empty json object
-            with open(self.json_file, 'w') as f:
-                f.write('{}')
-            # add the data to the memory object
-            with open(self.json_file, 'r') as f:
-                self.data = json.load(f)
+        super().__init__(kv_file)
 
     '''
         returns the json string
     '''
-
-    def get_all(self):
-        return self.data
-
-    '''
-        get a specific key from memory
-    '''
-
-    def get_data(self, key):
-        try:
-            return self.data[key]
-        except BaseException:
-            return None
-
-    '''
-        add a key and value to memory
-    '''
-
-    def add_data(self, key, value):
-        if self.get_data(key) is not None:
-            print("data already exists with that name")
-        else:
-            self.data[key] = value
-
-    '''
-        Updates a key with supplied value.
-    '''
-
-    def update_data(self, key, value):
-        self.data[key] = value
-
-    '''
-        delete a key from memory
-    '''
-
-    def del_data(self, key):
-        try:
-            del self.data[key]
-        except KeyError:
-            pass
-
-    '''
-        !!!!DANGER!!!!!
-        deletes the entire memory and overwrites the file with a blank file
-        only use when absolutely needed.
-    '''
-
-    def del_all(self):
-        with open(self.json_file, 'w') as f:
-            f.write('')
-
-    '''
-        Saves memory to disk. This must be ran before memory object
-        is destroyed. Otherwise all changes will be lost.
-    '''
-
-    def save(self):
-        with open(self.json_file, 'w') as f:
-            json.dump(self.data, f)
 
     def generate_secret_key(self):
         secret_key = self.get_data("secret_key")
