e672d06ba980504e8dc35d45250f515c6619f2f2
IriannaPapoutsi <irianna_pap@hotmail.gr>
2021-06-24 19:20:37 +0300

    Improve calories plugin

--- jarviscli/plugins/calories.py
+++ jarviscli/plugins/calories.py
@@ -1,13 +1,14 @@
+from colorama import Fore
 from plugin import plugin
 
 
 @plugin("calories")
 class calories:
     """
-    Tells the recommended daily calorie intake, also recommends
-    calories for weight add and loss.(Source 1)
-    It is based on gender, age, height and weight.
-    Uses the Miffin-St Jeor Equation as it is considered the
+    A Jarvis plugin to calculate and print the recommended daily calorie
+    intake,calories for weight add and loss.
+    The calculating method is based on gender, age, height and weight.
+    since it uses the Miffin-St Jeor Equation as it is considered the
     most accurate when we don't know our body fat percentage(Source 2).
     Add gender(man/woman), age(15 - 80 recommended), metric height(cm),
     weight(kg), workout level(1-4). No decimal weight for now.
@@ -23,41 +24,143 @@ class calories:
     """
 
     def __call__(self, jarvis, s):
-        jarvis.say("Welcome!")
-        info = input("Please enter the information about you following this order(gender age height(cms) weight(kg) workout level(1-4)): ")
-        self.calories(jarvis, info)
+        """
+        Calls the needed methods to validate the values that need to be parsed
+        as arguments to calories method. Afterwards, the last mentioned method
+        is called in order to calculate the calorie intake data.
+        """
+        jarvis.say("Hello there! To calculate your daily calorie intake "
+                   "I need to get to know you a bit more...")
+        # Reads the input gender and calls the validation method.
+        gender = jarvis.input("What's your gender? (M/F) ")
+        while not self.validate_gender(gender):
+            print(Fore.YELLOW + "Sorry, invalid input was given! Please"
+                                " try again. (M/F) ")
+            gender = jarvis.input()
+        # Reads the input age and calls the validation method.
+        age = jarvis.input("What's your age? ")
+        while not self.validate_age(age):
+            age = jarvis.input()
+        age = int(age)
+        # Reads the input height and calls the validation method.
+        height = jarvis.input("What is your height? (cm) ")
+        while not self.validate_height(height):
+            height = jarvis.input()
+        height = int(height)
+        # Reads the input weight and calls the validation method.
+        weight = jarvis.input("What is your weight? (kg) ")
+        while not self.validate_weight(weight):
+            weight = jarvis.input()
+        weight = int(weight)
+        jarvis.say("Choose your workout level (1-4) ")
+        message = Fore.RESET + Fore.YELLOW + "\nWorkout Levels:\n[1]" + \
+            Fore.RESET + " Little or no exercise\n"
+        message += Fore.RESET + Fore.YELLOW + "[2]" + Fore.RESET + \
+            " Light 1-3 per week\n"
+        message += Fore.RESET + Fore.YELLOW + "[3]" + Fore.RESET + \
+            " Moderate 4-5 per week\n"
+        message += Fore.RESET + Fore.YELLOW + "[4]" + \
+            Fore.RESET + " Active daily exercise or physical job"
+        jarvis.say(message)
+        # Reads the input workout_level and calls the validation method.
+        workout_level = jarvis.input("Please enter your choice: ")
+        while not self.validate_workout_level(workout_level):
+            workout_level = jarvis.input()
+        workout_level = int(workout_level)
+        # Calls the calories method to calculate the daily intake
+        brm_info = self.calories(gender, age, height, weight, workout_level)
+        jarvis.say("\nYour personal calorie data!", Fore.CYAN)
+        jarvis.say("Daily calorie intake:    " + Fore.RESET +
+                   Fore.GREEN + str(brm_info[0]))
+        jarvis.say("Loss weight calories:    " + Fore.RESET +
+                   Fore.YELLOW + str(brm_info[1]))
+        jarvis.say("Put on  weight calories: " + Fore.RESET +
+                   Fore.RED + str(brm_info[2]))
 
-    def calories(self, jarvis, info):
-        strings = info.split()
-        if len(strings) == 5:
-            gender = strings[0].lower()
-            age = int(strings[1])
-            height = int(strings[2])
-            weight = float(strings[3])
-            level = int(strings[4])
-        else:
-            jarvis.say("You wrote less or more arguments than it needed.")
-            return None
+    def validate_gender(self, gender):
+        """Function that takes as input the gender and validates it."""
+        # ignore lower or upper letters
+        return (gender.upper() == "M" or gender.upper() == "F")
 
-        gender_no = 0
-        if(gender == "male" or gender == "man" or gender == "m"):
+    def validate_age(self, age):
+        """Function that takes as input the age and validates it."""
+        try:
+            age = int(age)
+            if age <= 0 or age < 14:
+                raise ValueError
+            else:
+                return True
+        except ValueError:
+            print(Fore.YELLOW + "Oops! That was no valid number."
+                                " Try again...")
+            return False
+
+    def validate_height(self, height):
+        """Function that takes as input the height and validates it."""
+        try:
+            height = int(height)
+            if height <= 0:
+                raise ValueError
+            else:
+                return True
+        except ValueError:
+            print(Fore.YELLOW + "Oops! That was no valid input."
+                                " Try again...")
+            return False
+
+    def validate_weight(self, weight):
+        """Function that takes as input the weight and validates it."""
+        try:
+            weight = int(weight)
+            if weight <= 0:
+                raise ValueError
+            else:
+                return True
+        except ValueError:
+            print(Fore.YELLOW + "Oops! That was no valid input. Try again...")
+            return False
+
+    def validate_workout_level(self, workout_level):
+        """Function that takes as input the workout level and validates it."""
+        try:
+            workout_level = int(workout_level)
+            if workout_level <= 0 or workout_level > 4:
+                raise ValueError
+            else:
+                return True
+        except ValueError:
+            print(Fore.YELLOW + "Oops! That was no valid input. Try again...")
+            return False
+
+    def calories(self, gender, age, height, weight, workout_level):
+        """
+        Given the gender, age, height, weight and workout level arguments
+        the daily calorie intake is calculated based on the
+        Miffin-St Jeor Equation above mentioned method.
+        Three specialized rates of daily calorie intake are calculated:
+        the intake that preserves one's weight, the one to lose weight
+        and the intake to gain more weight.
+        """
+        if gender.lower == "m":
+            # A constant value based on gender
             gender_no = 5
-        elif(gender == "female" or gender == 'woman' or gender == "f"):
-            gender_no = -161
-
-        if gender_no != 0 and age > 14 and height > 0.0 and weight > 0.0 and level > 0 and level < 5:
-            brm = float(10 * weight + 6.25 * height - 5
-                        * age + gender_no) * self.exercise_level(level)
-            brm_loss = brm - 500.0
-            brm_put_on = brm + 500.0
-            jarvis.say("Daily caloric intake :    " + str(brm))
-            jarvis.say("Loss weight calories :    " + str(brm_loss))
-            jarvis.say("Put on  weight calories : " + str(brm_put_on))
         else:
-            jarvis.say("Please add correct input!")
-            return None
+            # A constant value based on gender
+            gender_no = -161
+        # The calculation of the intake that preserves weight
+        brm = float(10 * weight + 6.25 * height - 5
+                    * age + gender_no) * self.exercise_level(workout_level)
+        # The calculation of the intake to lose weight
+        brm_loss = brm - 500.0
+        # The calculation of the intake to gain weight
+        brm_put_on = brm + 500.0
+        return brm, brm_loss, brm_put_on
 
     def exercise_level(self, level):
+        """
+        Implements needed calculations for the calorie intake
+        calculation method based on the workout level
+        """
         multipliers = {1: 1.2, 2: 1.4, 3: 1.6, 4: 1.95}
         multiplier = multipliers.get(level, 1)
         return multiplier
