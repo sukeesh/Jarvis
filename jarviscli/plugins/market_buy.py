79b84508c4ae9e023fac6371a3a36c514025e1f4
Adriel Martins <adrielfalcao@gmail.com>
2021-06-26 02:47:22 -0300

    feat: adding the hability to mantain only python scripts and also making the plugin OS agnostic, and making sure that you can reinstall plugins

--- jarviscli/plugins/market_buy.py
+++ jarviscli/plugins/market_buy.py
@@ -1,18 +1,28 @@
 import os
+import shutil
+import git
 from plugin import Platform, plugin, require
 
 
-@require(network=True, platform=[Platform.LINUX])
+@require(network=True)
 @plugin('market buy')
 class market_buy():
     """
-    Install baskets of plugins from Github Topics. It accepts an
-    Github_user/Github_repo that has the tag 'jarvis-plugin'.
+    Install baskets of plugins from Github Topics.
 
     Check the PLUGIN_MARKETPLACE.md for more information.
     """
-
-    MARKETPLACE_PATH = 'marketplace'
+    # Absolute path for Jarvis/marketplace
+    MARKETPLACE_PATH = os.path.join((os.sep).join(os.path.normpath(__file__).split(os.sep)[0:-3]),
+                                    'marketplace')
+    MAINTAIN_PYTHON_ONLY = True
+    
+    def cleanse_non_py(self, parent_folder):
+        for root, dirs, files in os.walk(parent_folder):
+            for f in files:
+                if '.py' not in f:
+                    os.unlink(os.path.join(root, f))
+        
 
     def __call__(self, jarvis, s):
         homepage = 'https://github.com/'
@@ -24,7 +34,11 @@ class market_buy():
         plugin_dir = os.path.join(self.MARKETPLACE_PATH, s)
         os.makedirs(plugin_dir, exist_ok=True)
         try:
-            os.system('cd ' + plugin_dir + ' && git clone ' + repo_link + ' .')
+            target_dir = os.path.join(self.MARKETPLACE_PATH, s.split('/')[0])
+            if os.path.isdir(target_dir):
+                shutil.rmtree(target_dir)
+                os.makedirs(plugin_dir)
+            git.Git(target_dir).clone(repo_link)
         except ValueError as e:
             e('Please input a valid GitHub_User/Github_Repo.')
         os.system('cd ' + plugin_dir + ' && ls -l')
@@ -35,9 +49,16 @@ class market_buy():
             else:
                 for f in files:
                     if f == 'requirements.txt':
-                        os.system('env/bin/pip install -U -r ' + os.path.join(root, f))
+                        jarvis_folder = os.path.split(self.MARKETPLACE_PATH)[0]
+                        os.system((f'cd {jarvis_folder} && ' +
+                                   'env/bin/pip install -U -r ' +
+                                   os.path.join(root, f)))
         
         # Deleting the .git folder from the cloned repo.
-        os.rmdir(os.path.join(plugin_dir, '.git'))
+        shutil.rmtree(os.path.join(plugin_dir, '.git'))
+        
+        if self.MAINTAIN_PYTHON_ONLY:
+            self.cleanse_non_py(plugin_dir)
         
         jarvis.say('Operation completed. Please restart Jarvis.')
+        
