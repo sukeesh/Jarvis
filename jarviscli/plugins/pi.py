c76e0db6e132c699a89d042aea436b3f443dfc79
Aurélien Buchet <45171721+AurelienBuchet@users.noreply.github.com>
2021-06-30 14:53:03 +0200

    Add plugins for number convertions (#1)
    * add a prime plugin
    
    * reformat prime.py
    
    * update pi.py
    
    * add a revert binary and hexa
    
    * add rev_hex
    
    Co-authored-by: Alexandre <alexandreconde@free.fr>
    Co-authored-by: gdeside <guillaume.deside@student.uclouvain.be>

--- jarviscli/plugins/pi.py
+++ jarviscli/plugins/pi.py
@@ -10,17 +10,17 @@ NUM_NEXT = 100
 
 @plugin("pi")
 def next_pi(jarvis, s):
-    jarvis.say("Pi equals 3.14...", Fore.GREEN)
-    # today = datetime.date.today()
-
-    # print(today)
-
-    pi_file = open(os.path.join(FILE_PATH, '../data/pi.txt'), 'r')
-    pi_number = pi_file.read()
-    index = 4
-    while True:
-        user_input = jarvis.input("Enter \'n\' to print next {} digits : ".format(str(NUM_NEXT)))
-        if (user_input != 'n'):
-            break
-        jarvis.say(pi_number[index: index + NUM_NEXT], Fore.GREEN)
-        index += NUM_NEXT
+    """
+    Give the number of digit for pi
+    """
+    if s == "":
+        s = jarvis.input("Number of digit after dot? ")
+    try:
+        n = int(s)
+    except ValueError:
+        jarvis.say("That's not a number")
+        return
+    else:
+        pi_file = open(os.path.join(FILE_PATH, '../data/pi.txt'), 'r')
+        pi_number = pi_file.read()
+        jarvis.say(pi_number[0:n + 2], Fore.GREEN)
