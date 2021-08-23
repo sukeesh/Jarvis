a1b8736638fbd73d5f730a9db4ad74ebbe7dc408
AhmetFurkann <35719856+AhmetFurkann@users.noreply.github.com>
2021-08-23 14:24:43 +0300

    Add resource downloader for modules

--- installer/steps/b_pip.py
+++ installer/steps/b_pip.py
@@ -6,3 +6,11 @@ section("Install requirements")
 CMD = "{} install -U -r installer/requirements.txt"
 CMD = CMD.format(unix_windows.VIRTUALENV_PIP)
 shell(CMD).should_not_fail()
+
+section("Download resource requirements")
+res_requirements = open('installer/res_requirements.txt', 'r')
+for res_name in res_requirements:
+    CMD = unix_windows.VIRTUALENV_PIP_DOWNLOAD
+    CMD = CMD + " " + res_name
+    shell(CMD).should_not_fail()
+res_requirements.close()
