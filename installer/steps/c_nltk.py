7df396b202c1159171c31eacfaab20cb66667822
Milind Sharma <milindsharma8@gmail.com>
2021-11-07 20:11:58 +0800

    Make grammatical improvements to print msgs

--- installer/steps/c_nltk.py
+++ installer/steps/c_nltk.py
@@ -4,7 +4,7 @@ import unix_windows
 
 PACKAGES = ["wordnet", "punkt"]
 
-section("Download additional data (Dictionary)")
+section("Downloading additional data (Dictionary)")
 CMD = '{} -m nltk.downloader -d jarviscli/data/nltk {{}}'
 CMD = CMD.format(unix_windows.VIRTUALENV_PYTHON)
 
