0da6522e99081ab3399008f41c34076e1c82f42b
pnhofmann <foss@pnhofmann.de>
2022-01-18 13:32:08 +0100

    Remove scrot installation within plugin

--- jarviscli/plugins/screencapture.py
+++ jarviscli/plugins/screencapture.py
@@ -1,33 +1,14 @@
-from plugin import plugin, require, alias
 import os
-import sys
-import subprocess
-import platform
+
 import pyautogui
+
+from plugin import alias, plugin, require
+
 
 @require(native='scrot')
 @alias("capture", "screen")
 @plugin('screen_capture')
 def capture_screen(jarvis, s):
-
-    if (platform.system() == 'Linux'):
-        proc = subprocess.Popen(['dpkg',
-                                 '-s',
-                                 'scrot'],
-                                stdin=subprocess.PIPE,
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.PIPE)
-        out, _ = proc.communicate()
-        if not(out.decode("utf-8")):
-            print("Package Not Installed.")
-            print("***************************\n")
-
-            print(
-                "Please install the following package for proper functioning of the screenshot function: \n")
-            print("1. 'sudo apt-get install scrot'\n")
-
-            print("***************************\n")
-            sys.exit()
     jarvis.say("Please specify the path: ")
     path = jarvis.input()
     image = pyautogui.screenshot()
