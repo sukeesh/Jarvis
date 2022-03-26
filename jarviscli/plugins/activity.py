693e2bac7fb59b8e8c29e32ab3034f9727c821d1
Alex Doganis <72792044+AirlineDog@users.noreply.github.com>
2022-03-26 18:37:15 +0200

    Fix string formatting for activity plugin (#938)

--- jarviscli/plugins/activity.py
+++ jarviscli/plugins/activity.py
@@ -16,22 +16,22 @@ def activity(jarvis, s):
         jarvis.say("Sorry, an error occured", Fore.BLUE)
         return
 
-    if (data.get('accessibility') < 0.4):
+    if data.get('accessibility') < 0.4:
         response += "I can purpose something interesting but it's not easy and "
-    elif (data.get('accessibility') < 0.6):
+    elif data.get('accessibility') < 0.6:
         response += "I have something easy for you and "
 
-    if (data.get('price') == 0):
+    if data.get('price') == 0:
         response += "it's free, "
-    elif (data.get('price') <= 0.3):
+    elif data.get('price') <= 0.3:
         response += "it's quite cheap, "
-    elif (data.get('price') > 0.3 and data.get('price') < 0.6):
+    elif 0.3 < data.get('price') < 0.6:
         response += "it cost a few. "
-    elif (data.get('price') >= 0.6):
+    elif data.get('price') >= 0.6:
         response += "but it's expensive. "
 
-    if (data.get('participants') > 1):
-        response += "It's a group activity of" + str(data.get('participants')) + "participants"
+    if data.get('participants') > 1:
+        response += "It's a group activity of " + str(data.get('participants')) + " participants. "
 
-    response += data.get('activity')
+    response += data.get('activity') + "."
     jarvis.say(response, Fore.BLUE)
