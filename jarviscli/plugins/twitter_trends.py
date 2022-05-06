4131002344f2c2c6d3db5d98dd95ccd4ef332174
LeonidhaMara <t8190101@aueb.gr>
2022-05-06 20:06:26 +0300

    Add template for twitter trends plugin

---
+++ jarviscli/plugins/twitter_trends.py
@@ -0,0 +1,19 @@
+
+class Trend:
+    pass
+
+
+def available_countries():
+    pass
+
+
+def show_countries():
+    pass
+
+
+def get_country():
+    pass
+
+
+def get_trends(country: int):
+    pass
