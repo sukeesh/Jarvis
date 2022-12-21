15fbd7ad3f96c1ae25a1f10e4c881d7816a5d02e
PauloMRocha <paulo.mrocha@usp.br>
2022-12-20 22:13:18 -0300

    Add plugin to calc. weekday of ane date

---
+++ jarviscli/plugins/weekday.py
@@ -0,0 +1,81 @@
+from plugin import alias, plugin
+from colorama import Fore
+
+def dayofweek(d, m, y):
+    """
+    Calculates the weekday for any date and returns 0 to Sunday, 1 to Monday...
+    """
+    # code from https://www.geeksforgeeks.org/find-day-of-the-week-for-a-given-date/
+    t = [ 0, 3, 2, 5, 0, 3,
+          5, 1, 4, 6, 2, 4 ]
+    y -= m < 3
+    return (( y + int(y / 4) - int(y / 100)
+             + int(y / 400) + t[m - 1] + d) % 7)
+
+@alias("weekday")
+@plugin("day of the week")
+def weekday(jarvis, s):
+    """
+    Says what weekday any date is.
+    """
+    m_with_31 = [1,3,5,7,8,10,12]
+
+    if s == "":
+        s = jarvis.input("Tell me any date using the dd/mm/aaaa format:\n")
+
+    # wrong format error
+    s_split = s.split("/")
+    if(len(s)<3):
+        jarvis.say("Something is worng with this date.\n Try to use \"weekday dd/mm/aaaa\"", Fore.ORANGE)
+        return
+
+    # splitting data and turning into int
+    d = int(s_split[0])
+    m = int(s_split[1])
+    a = int(s_split[2])
+
+    if a<1:
+        jarvis.say("Sorry, but I only can the the day of the week from year 1 onwards! :(", Fore.RED)
+        return
+
+    if m<1 or m>12:
+        jarvis.say("Months go from 1 to 12! Remember to use dd/mm/aaaa format.", Fore.RED)
+        return
+
+    if d<1 or d>31:
+        jarvis.say("Days go from 1 to 31! Remember to use dd/mm/aaaa format.", Fore.RED)
+        return
+
+    if d==31 and m not in m_with_31:
+        jarvis.say(f"Month {m} doen't have 31 days!", Fore.RED)
+        return
+
+    if m==2 and d>29:
+        jarvis.say("Month 2 have only 28 or 29 days!", Fore.RED)
+        return
+
+    # calculate weekday
+    week_day = dayofweek(d, m, a)
+
+    if week_day == 0:
+        jarvis.say(f"{s} is a Sunday!", Fore.GREEN)
+
+    elif week_day == 1:
+        jarvis.say(f"{s} is a Monday!", Fore.GREEN)
+
+    elif week_day == 2:
+        jarvis.say(f"{s} is a Tuesday!", Fore.GREEN)
+
+    elif week_day == 3:
+        jarvis.say(f"{s} is a Wednesday!", Fore.GREEN)
+
+    elif week_day == 4:
+        jarvis.say(f"{s} is a Thursday!", Fore.GREEN)
+
+    elif week_day == 5:
+        jarvis.say(f"{s} is a Friday!", Fore.GREEN)
+
+    elif week_day == 6:
+        jarvis.say(f"{s} is a Saturday!", Fore.GREEN)
+
+    return
