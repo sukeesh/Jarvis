aecbfaee7be8058a58f9c0251051ee9b054ac4ea
benibuehler <beni.buehler@bluewin.ch>
2023-02-01 21:17:30 +0100

    reworked endofyear

--- jarviscli/plugins/endofyear_timer.py
+++ jarviscli/plugins/endofyear_timer.py
@@ -1,47 +1,47 @@
-from plugin import plugin, alias
-from datetime import datetime
+from plugin import plugin
+from colorama import Fore
+import datetime
+
+@plugin("endofyear")
+def endofyear(jarvis, s):
+    """
+    Tells the time till the end of the year
+    """
+
+    actual_datetime = datetime.datetime.today()
+    end_of_year = datetime.datetime(actual_datetime.year, 12, 31, 23, 59, 59)
+
+    time_till_end = end_of_year - actual_datetime
+
+    jarvis.say(f"It\'s {format_time_delta(time_till_end)}" +
+        " until the end of the year", Fore.YELLOW)
 
 
-@alias("endofyear")
-@plugin("endofyear timer")
-class EndOfYearTimer:
-    def __call__(self, jarvis, s):
-        self.jarvis = jarvis
-        self.main()
+def format_time_delta(t) -> str:
+    remaining_secs = t.seconds % 3600
+    time_dict = {
+        'days' : t.days,
+        'hours' : int(t.seconds / 3600),
+        'minutes' : int(remaining_secs / 60),
+        'seconds' : remaining_secs % 60
+    }
 
-    def main(self):
-        actual_datetime = datetime.now()
-        end_of_year = datetime(actual_datetime.year, 12, 31, 23, 59, 59)
+    new_timedict = {}
+    # create new dictionary with the keys being the right numeric value
+    for element in time_dict:
+        if time_dict[element] == 1:
+            new_timedict[element[0:len(element)-1]] = time_dict[element]
+        else:
+            new_timedict[element] = time_dict[element]
 
-        time_till_end = end_of_year - actual_datetime
+    # store keys and values in a list
+    # for easier access
+    measures = list(new_timedict.keys())
+    values = list(new_timedict.values())
+    
+    timedelta = ''
+    for i in range(len(values)):
+        timedelta += f'{values[i]} {measures[i]}, '
 
-        self.jarvis.say(f"It\'s {self.format_time_delta(time_till_end)}" +
-            " until the end of the year")
-
-    def format_time_delta(self, t) -> str:
-        remaining_secs = t.seconds % 3600
-        time_dict = {
-            'days' : t.days,
-            'hours' : int(t.seconds / 3600),
-            'minutes' : int(remaining_secs / 60),
-            'seconds' : remaining_secs % 60
-        }
-
-        new_timedict = {}
-        for element in time_dict:
-            if time_dict[element] == 1:
-                new_timedict[element[0:len(element)-1]] = time_dict[element]
-            else:
-                new_timedict[element] = time_dict[element]
-
-        # store keys and values in a list
-        # for easier access
-        days = list(new_timedict.keys())
-        values = list(new_timedict.values())
-        
-        timedelta = ''
-        for i in range(len(values)):
-            timedelta += f'{values[i]} {days[i]}, '
-
-        # ignore the last 2 characters ', '
-        return timedelta[:-2]
+    # ignore the last 2 characters ', '
+    return timedelta[:-2]
