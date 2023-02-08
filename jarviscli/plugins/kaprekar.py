76eaa3fcea2d4a468d2ad28e26933230e8359c2a
mkatsara <92687810+mkatsara@users.noreply.github.com>
2023-02-08 05:55:05 +0200

    Add files via upload
    Give a number and check if it is Kaprekar number or not.

---
+++ jarviscli/plugins/kaprekar.py
@@ -0,0 +1,25 @@
+from plugin import plugin
+
+
+@plugin("kaprekar")
+def kaprekar(jarvis, s):
+    n1 = int(input("Enter a number: "))
+    if n1 > 0:
+        sq = n1 * n1
+        # print(sq)
+        m = 1
+        m = m * 10
+        a = sq // m
+        b = sq % m
+        while a + b != n1 and a != 0:
+            m = m * 10
+            a = sq // m
+            b = sq % m
+
+        if a + b == n1 and sq % 10 != 0:
+            answer = "Yes, kaprekar number"
+        else:
+            answer = "Not kaprekar number"
+        jarvis.say(answer)
+
+
