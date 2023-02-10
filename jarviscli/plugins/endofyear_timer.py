c7575a184e612a3a349098a9d543791b843a400b
benibuehler <beni.buehler@bluewin.ch>
2023-02-10 21:11:09 +0100

    Add christmas timer and centralized formatter for timedelta

--- jarviscli/plugins/endofyear_timer.py
+++ jarviscli/plugins/endofyear_timer.py
@@ -1,5 +1,6 @@
 from plugin import plugin
 from colorama import Fore
+from utilities.timedeltaUtilities import Timedelta_utilities
 import datetime
 
 @plugin("endofyear")
@@ -13,35 +14,5 @@ def endofyear(jarvis, s):
 
     time_till_end = end_of_year - actual_datetime
 
-    jarvis.say(f"It\'s {format_time_delta(time_till_end)}" +
+    jarvis.say(f"It\'s {Timedelta_utilities.format_time_delta(time_till_end)}" +
         " until the end of the year", Fore.YELLOW)
-
-
-def format_time_delta(t) -> str:
-    remaining_secs = t.seconds % 3600
-    time_dict = {
-        'days' : t.days,
-        'hours' : int(t.seconds / 3600),
-        'minutes' : int(remaining_secs / 60),
-        'seconds' : remaining_secs % 60
-    }
-
-    new_timedict = {}
-    # create new dictionary with the keys being the right numeric value
-    for element in time_dict:
-        if time_dict[element] == 1:
-            new_timedict[element[0:len(element)-1]] = time_dict[element]
-        else:
-            new_timedict[element] = time_dict[element]
-
-    # store keys and values in a list
-    # for easier access
-    measures = list(new_timedict.keys())
-    values = list(new_timedict.values())
-    
-    timedelta = ''
-    for i in range(len(values)):
-        timedelta += f'{values[i]} {measures[i]}, '
-
-    # ignore the last 2 characters ', '
-    return timedelta[:-2]
