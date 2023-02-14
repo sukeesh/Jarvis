86c7fb90c5bcd93ed23b576a3d60bf937e146d67
giorgos pappas <pappasgc@csd.auth.gr>
2023-02-14 13:58:07 +0200

    armstrong number feature

---
+++ jarviscli/plugins/armstrong_numbers.py
@@ -0,0 +1,19 @@
+from plugin import plugin
+
+
+@plugin("armstrong_numbers")
+def element(jarvis, s):
+    num = int(input("Enter a number: "))
+    length = len(str(num))
+    sum = 0
+    tmp = num
+    while tmp > 0:
+        digit = tmp % 10
+        sum += digit ** length
+        tmp //= 10
+
+    if num == sum:
+        answer = "The number " + num + " is armstrong."
+    else:
+        answer = "The number " + num + " is not  armstrong."
+    jarvis.say(answer)
