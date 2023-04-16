5ad49770a8abd11d0d9b1cb414d2c71c45424cff
prophecyprogrammer <vatsal.shah2304@gmail.com>
2023-04-16 21:52:19 +0530

    Now the gui window destroys after 3 seconds

--- jarviscli/plugins/spinthewheel.py
+++ jarviscli/plugins/spinthewheel.py
@@ -6,7 +6,7 @@ import math
 
 def create_wheel(sectors):
     """
-    Creates GUI of a spinwheel, previous code was simply used to show the output in the terminal.
+    Creates GUI of a spinwheel and closes the window after 5 seconds.
 
     """
     # Define the size of the canvas and the radius of the wheel
@@ -39,22 +39,26 @@ def create_wheel(sectors):
                           width=2, style='pie')
         canvas.create_text(center_x, center_y, text=sectors[i], font=('Arial', 12, 'bold'))
         start_angle = end_angle
-        # Draw an arrow pointing to the center of the circle
-        # Calculate the angle for the selected sector
+
+    # Draw an arrow pointing to the center of the circle
+    # Calculate the angle for the selected sector
     selected_sector = sectors.index(spinit(sectors))
     selected_angle = selected_sector * sector_angle + sector_angle / 2
 
-        # Calculate the coordinates of the base of the arrow
+    # Calculate the coordinates of the base of the arrow
     arrow_base_x = canvas_width / 2
     arrow_base_y = canvas_height / 2
 
-        # Calculate the coordinates of the tip of the arrow
+    # Calculate the coordinates of the tip of the arrow
     arrow_tip_x = arrow_base_x + (wheel_radius * 0.4) * math.sin(math.radians(selected_angle))
     arrow_tip_y = arrow_base_y - (wheel_radius * 0.4) * math.cos(math.radians(selected_angle))
 
-        # Draw the arrow
+    # Draw the arrow
     canvas.create_line(arrow_base_x, arrow_base_y, arrow_tip_x, arrow_tip_y, fill='red', width=10, arrow='last',
-                           arrowshape=(15, 20, 5))
+                       arrowshape=(15, 20, 5))
+
+    # Schedule the window to close after 5 seconds
+    root.after(3000, root.destroy)
 
     # Start the tkinter event loop
     root.mainloop()
