26dc758aa164599eb89480313145021d83db6134
gaganchandan <gaganchandan29@gmail.com>
2023-04-29 12:45:57 +0000

    Add Wordle plugin

--- jarviscli/plugins/wordle_game.py
+++ jarviscli/plugins/wordle_game.py
@@ -5,7 +5,7 @@ from plugin import plugin
 
 @plugin("wordle")
 class Wordle():
-    def __call__(self, jarvis, s): 
+    def __call__(self, jarvis, s):
         logo = '''
  .----------------.  .----------------.  .----------------.  .----------------.  .----------------.  .----------------. 
 | .--------------. || .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |
@@ -18,46 +18,44 @@ class Wordle():
 | |              | || |              | || |              | || |              | || |              | || |              | |
 | '--------------' || '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |
  '----------------'  '----------------'  '----------------'  '----------------'  '----------------'  '----------------' '''
-        data_path = os.path.abspath(os.path.dirname(__file__))      
-        data = data_path + '/words_folder/words'
-        lists_of_words = list(map(lambda x : x.split()[3],f.readlines()))
+        data_path = os.path.abspath(os.path.dirname(__file__))
+        data = data_path[:-8] + '/data/wordle.txt'
+        f = open(data, 'r')
+        lists_of_words = list(map(lambda x: x.split()[3], f.readlines()))
         wordToGuess = choice(lists_of_words)
-        if '\n' in wordToGuess:
-            wordToGuess[-1] = ''
-
         jarvis.say(logo)
-
-        jarvis.say("Welcome to wordle! You get 5 chances to guess the 5 letter word...")
+        jarvis.say(
+            "Welcome to wordle! You get 5 chances to guess the 5 letter word...")
         jarvis.say("After each guess it shows you the result of your answer.")
-        jarvis.say("\tG - right position and right letter.\n\tY - right letter wrong position.\n\tW - wrong letter.")
+        jarvis.say(
+            "\tG - right position and right letter.\n\tY - right letter wrong position.\n\tW - wrong letter.")
 
         jarvis.say("\nLet's start the game!")
 
         lives = 5
-        while(lives != 0):
+        while (lives != 0):
             answerByTheUser = jarvis.input("Enter the guess: ")
-            lives -= lives
+            lives -= 1
 
             if lives == 0:
-                jarvis.say(f"Uh-oh, you are out of lives.\nThe correct word was: \"{wordToGuess}\".")
+                jarvis.say(
+                    f"Uh-oh, you are out of lives.\nThe correct word was: \"{wordToGuess}\".")
                 jarvis.say("Better luck next time! ")
 
-
-            elif answerByTheUser == wordToGuess:
+            elif answerByTheUser.lower() == wordToGuess.lower():
                 jarvis.say("Congratulaions! You guessed it right!\n")
-                jarvis.say("Lives left: ", lives)
+                jarvis.say("Lives left: {}".format(lives))
                 break
 
             else:
-                for index in range(0,5):
-                    if answerByTheUser[index] == wordToGuess[index]:
-                        jarvis.say("G ", Fore = 'green')
+                for index in range(0, 5):
+                    if answerByTheUser[index].lower() == wordToGuess[index].lower():
+                        jarvis.say("G ")
 
-                    elif answerByTheUser[index] in list(wordToGuess):
-                        jarvis.say("Y ", Fore = 'yellow')
+                    elif answerByTheUser[index].lower() in list(wordToGuess.lower()):
+                        jarvis.say("Y ")
 
                     else:
-                        jarvis.say("W ", Fore = 'red')
+                        jarvis.say("W ")
                 jarvis.say("Wrong guess...\nTry again: ")
-                jarvis.say("Lives left: ", lives)
-
+                jarvis.say("Lives left: {}".format(lives))
