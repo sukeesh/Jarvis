5c0d659219b6787225ad58a379a54735916aa099
Om  Patil <omsandeeppatil02@gmail.com>
2024-01-07 16:12:25 +0530

    Update helper.py

--- installer/helper.py
+++ installer/helper.py
@@ -7,89 +7,63 @@ from threading import Thread
 from tempfile import NamedTemporaryFile
 from unix_windows import IS_WIN
 
-SUPPORTED_SHELLS = [
-    'bash',
-    'zsh',
-]
+SUPPORTED_SHELLS = ['bash', 'zsh']
 
-# python 2/3 compatibility
-try:
-    input = raw_input
-except NameError:
-    pass
+class Logger:
+    def __init__(self):
+        self.debug_log = None
 
+    def init_log(self):
+        self.debug_log = NamedTemporaryFile(delete=False, mode="w")
+        print(f"Logging to {self.debug_log.name}")
 
-def executable_exists(name):
-    binary_path = shutil.which(name)
-    return binary_path is not None and os.access(binary_path, os.X_OK)
+    def log(self, msg):
+        try:
+            self.debug_log.write(f"{msg}\n")
+        except Exception as e:
+            self.print_log('------------------------------')
+            print("Logging failed?")
+            print(repr(e))
+            print(str(e))
+            print(str(e.args))
+            print('msg:')
+            try:
+                print(str(msg))
+            except BaseException:
+                print('msg unprintable')
+            print('-----------------------------')
 
+    def print_log(self, msg):
+        self.log(msg)
+        print(msg)
 
-debug_log = None
+    def close_log(self):
+        self.debug_log.close()
 
-
-def log_init():
-    global debug_log
-    debug_log = NamedTemporaryFile(delete=False, mode="w")
-    print("Logging to {}".format(debug_log.name))
-
-
-def log_close():
-    debug_log.close()
-
+logger = Logger()
 
 def fail(msg, fatal=False):
-    log("Installation failed")
-    log(msg)
+    logger.print_log("Installation failed")
+    logger.log(msg)
     print(msg)
     print('')
     print('')
     if fatal:
-        log("FATAL!")
-        print("Installation failed with unexpected error - This should not have happened.")
-        print("Please check logs at \"{}\". If you open a bug report, please include this file.".format(debug_log.name))
+        logger.log("FATAL!")
+        print("Installation failed with an unexpected error. This should not have happened.")
+        print(f"Please check logs at \"{logger.debug_log.name}\". If you open a bug report, please include this file.")
     else:
         print("Installation failed!")
-    debug_log.close()
+    logger.close_log()
     sys.exit(1)
 
-
-def log(msg):
-    try:
-        debug_log.write(msg)
-        debug_log.write('\n')
-    except Exception as e:
-        print('------------------------------')
-        print("Logging failed?")
-        print(repr(e))
-        print(str(e))
-        print(str(e.args))
-        print('msg:')
-        try:
-            print(str(msg))
-        except BaseException:
-            print('msg unprintable')
-        print('-----------------------------')
-
-
-def printlog(msg):
-    log(msg)
-    print(msg)
-
-
 def section(title):
-    printlog("\n{:=^50}".format(" {} ".format(title)))
-
-
-spinning = True
-
+    logger.print_log(f"\n{'{:=^50}'.format(f' {title} ')}")
 
 def spinning_cursor_start():
-    global spinning
-    spinning = True
-
     def spin_start():
         time.sleep(0.1)
-        while spinning:
+        while getattr(spinning_cursor_start, "spinning", True):
             for cursor in '|/-\\':
                 sys.stdout.write(cursor)
                 sys.stdout.flush()
@@ -98,105 +72,75 @@ def spinning_cursor_start():
 
     Thread(target=spin_start).start()
 
-
 def spinning_cursor_stop():
-    global spinning
-    spinning = False
-
+    spinning_cursor_start.spinning = False
 
 def user_input(items):
-    log("User input:")
+    logger.log("User input:")
     for x, item in enumerate(items):
-        printlog("{}) {}".format((x + 1), item[0]))
+        logger.print_log(f"{x + 1}) {item[0]}")
 
     while True:
-        number = input("Select number {}-{}: ".format(1, len(items)))
+        number = input(f"Select number 1-{len(items)}: ")
         try:
             number = int(number) - 1
         except ValueError:
-            log("> User input {} - not a number".format(number))
+            logger.log(f"> User input {number} - not a number")
             continue
 
-        if number >= 0 and number < len(items):
-            log("> User input {} - ok: {}".format(number, items[number][1]))
+        if 0 <= number < len(items):
+            logger.log(f"> User input {number} - ok: {items[number][1]}")
             return items[number][1]
         else:
-            log("> User input {} - out of range {} - {}".format(number, 1, len(items)))
+            logger.log(f"> User input {number} - out of range 1 - {len(items)}")
 
-def confirm_user_input(confirmation_message : str) -> bool:
-    log("User Confirmation")
-    printlog(confirmation_message)
-    confirm = input("input 'y' to confirm, 'n' to cancel: ")
-    confirm_bool = True if confirm in ['y', 'Y', 'yes', 'YES'] else False;
-    log("User chose to {} the operation.".format("cancel" if confirm_bool == False else "confirm"))
-    return confirm_bool
+ 
 
 def shell(cmd):
-    class Fail:
-        def should_not_fail(self, msg=''):
-            fail(msg, fatal=True)
+    class ShellException(Exception):
+        pass
 
-        def success(self):
-            return False
-
-        def __str__(self):
-            return "FAIL {}".format(self.exception)
-
-    class Success:
-        def should_not_fail(self, msg=''):
-            pass
-
-        def success(self):
-            return True
-
-        def __str__(self):
-            return "OK"
-
-    exit_code = Success()
-
-    log("_" * 40)
-    log("Shell: {}".format(cmd))
+    logger.log("_" * 40)
+    logger.log(f"Shell: {cmd}")
     spinning_cursor_start()
 
     cli_output = ''
     try:
         cli_output = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT, universal_newlines=True)
     except subprocess.CalledProcessError as e:
-        exit_code = Fail()
-        exit_code.exception = str(e)
-        cli_output += str(e.output)
+        raise ShellException(str(e), output=str(e.output))
 
-    # python 2 compatibility
+ 
     try:
         cli_output = cli_output.decode("utf-8")
     except AttributeError:
         pass
-    exit_code.cli_output = cli_output
 
-    log(cli_output)
-    log("Shell: Exit {}".format(str(exit_code)))
-    log("-" * 40)
-    log("")
+    logger.log(cli_output)
+    logger.log(f"Shell: Exit OK")
+    logger.log("-" * 40)
+    logger.log("")
 
     spinning_cursor_stop()
     time.sleep(0.5)
     sys.stdout.write(' \b')
 
-    return exit_code
-
+    return cli_output
 
 def get_default_shell():
-    '''
-    Determine and return the default shell
-    of the current logged in user.
-    Args:
-        None
-    Returns:
-        shell name (str)
-    '''
     try:
         bin_path = os.environ.get('SHELL')
         return bin_path.split(os.path.sep)[-1]
     except AttributeError:
-        # SHELL environment not set
         return None
+
+if __name__ == "__main__":
+    try:
+        logger.init_log()
+       
+    except ShellException as se:
+        fail(f"Shell command failed: {se}", fatal=True)
+    except Exception as e:
+        fail(f"An unexpected error occurred: {e}", fatal=True)
+    finally:
+        logger.close_log()
