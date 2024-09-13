406f12c0cfc443577cdefad679a1c92f22cd09e6
kartik-199 <kathuriakartik2005@gmail.com>
2024-09-13 12:41:56 -0400

    initial commitz

---
+++ jarviscli/plugins/soccer.py
@@ -0,0 +1,12 @@
+import requests
+from plugin import plugin, require
+from colorama import Fore
+from packages.memory.memory import Memory
+
+URL = "API_URL"
+API_KEY = "API_KEY"
+
+@require(network=True)
+@plugin("soccer")
+class soccer():
+    
