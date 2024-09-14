11387ec791a02faf729826cc85c19ddbae87e43f
kartik-199 <kathuriakartik2005@gmail.com>
2024-09-13 21:53:54 -0400

    Initial API call

--- jarviscli/plugins/soccer.py
+++ jarviscli/plugins/soccer.py
@@ -1,11 +1,21 @@
 import requests
-from plugin import plugin, require
-from colorama import Fore
-from packages.memory.memory import Memory
 
-URL = "API_URL"
-API_KEY = "API_KEY"
 
-@require(network=True)
-@plugin("soccer")
-class soccer():
+BASE_URL = "http://api.football-data.org/v4/"
+API_KEY = "0209778eaa6148d8b68b6484ff217a4f"
+
+class Soccer():
+    def get_competitions(self):
+        headers = {
+            'X-Auth-Token': API_KEY
+        }
+        response = requests.get(BASE_URL + 'competitions', headers=headers)
+        if response.status_code == 200:
+            payload = response.json()
+            competitions = payload['competitions']
+            for competition in competitions:
+                print(f"{competition['name']:<30} (ID: {competition['id']})")
+
+
+if __name__ == '__main__':
+    main()
