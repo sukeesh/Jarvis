0dae2767a08b3c308739ef80caa71ddb22b7fc36
Jakub-Haus <150725770+Jakub-Haus@users.noreply.github.com>
2024-10-16 13:18:07 +0200

    Update numbers_api.py
    new functionality added:
    numbersapi random trivia
    numbersapi random date
    numbersapi random year
    numbersapi random number
    
    numbersapi 1 2 3 still works like before

--- jarviscli/plugins/numbers_api.py
+++ jarviscli/plugins/numbers_api.py
@@ -7,8 +7,12 @@ from plugin import plugin, require
 @plugin("numbersapi")
 def numbersapi(jarvis, s):
     """
-    numbersapi: Displays API
-    numbersapi <list of numbers>: Displays facts about each number in list"
+    numbersapi: Displays API facts
+    numbersapi <list of numbers>: Displays facts about each number in list
+    numbersapi random trivia: Displays a random trivia fact
+    numbersapi random date: Displays a random fact about a date
+    numbersapi random year: Displays a random fact about a year
+    numbersapi random number: Displays a random number fact
     numbersapi help: Prints this help
 
     ** Data provided by: http://numbersapi.com/
@@ -17,6 +21,23 @@ def numbersapi(jarvis, s):
 
     if not s or 'help' in parts:
         jarvis.say(numbersapi.__doc__, Fore.GREEN)
+        return
+
+    if "random" in parts:
+        if "trivia" in parts:
+            data = get_data(jarvis, "random/trivia")
+        elif "date" in parts:
+            data = get_data(jarvis, "random/date")
+        elif "year" in parts:
+            data = get_data(jarvis, "random/year")
+        elif "number" in parts:
+            data = get_data(jarvis, "random")
+        else:
+            jarvis.say("\tInvalid random type. Use 'trivia', 'date', 'year', or 'number'.", Fore.RED)
+            return
+
+        if data:
+            jarvis.say(f"\t{data}", Fore.CYAN)
         return
 
     if any((not part.isnumeric() for part in parts)):
@@ -29,10 +50,10 @@ def numbersapi(jarvis, s):
             jarvis.say(f"\t{data}", Fore.CYAN)
 
 
-def get_data(jarvis, number):
+def get_data(jarvis, endpoint):
     base_url = "http://numbersapi.com/"
     try:
-        response = requests.get(f"{base_url}{number}")
+        response = requests.get(f"{base_url}{endpoint}")
         return response.text
     except requests.exceptions.RequestException:
         jarvis.say(f"\tCould not get data from {base_url}", Fore.RED)
