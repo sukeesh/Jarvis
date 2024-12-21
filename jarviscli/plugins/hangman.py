e44a9df3b87209e2a1d25a9e70dbc17ca274049c
evidencebp <idanamit@gmail.com>
2024-12-21 20:22:06 +0200

    jarviscli\plugins\hangman.py	 too-many-statements
    Function hangman had 67 statements while Pylint recommends having at most 50.
    I extracted methods to make the code more structured and solve that.

--- jarviscli/plugins/hangman.py
+++ jarviscli/plugins/hangman.py
@@ -4,88 +4,101 @@ from random_word import RandomWords
 
 @plugin('hangman')
 def hangman(jarvis, s):
+    print_hello()
+    terminateFlag = 0
+
+    while not terminateFlag:
+        lives = 8
+        usedLetters = ""
+        actualWordToGuess = get_word_to_guess()
+
+        wordToGuess = "_" * len(actualWordToGuess)
+        play(lives, usedLetters, actualWordToGuess, wordToGuess)
+
+        print("Word To Guess Was : " + actualWordToGuess.upper())
+        terminateFlag = continueOrNot()
+
+    print_goodbye()
+
+def play(lives, usedLetters, actualWordToGuess, wordToGuess):
+    while True:
+        if lives == 0:
+            print("You Lost!\n")
+            break
+        if actualWordToGuess == wordToGuess:
+            print("You Won!\n")
+            break
+        print("Word To Guess Looks Like This : " + wordToGuess + "\n")
+        guess = input("Enter Your Guess : ")
+        print("\n")
+        guess = guess.strip().lower()
+        if guess == "stop":
+            print("You Stopped Playing Hangman!\n")
+            break
+        if len(guess) == 0:
+            print("Woops! You Have Not Entered Anything\n")
+            lives = lives - 1
+            print("Penalty! Lives Decrease By 1, Remains : " + str(lives) + "\n")
+            drawStickMan(8 - lives)
+            continue
+        if len(guess) > 1:
+            print("Woops! You Have Entered Input Longer Than Character Size\n")
+            lives = lives - 1
+            print("Penalty! Lives Decrease By 1, Remains : " + str(lives) + "\n")
+            drawStickMan(8 - lives)
+            continue
+        if guess in usedLetters:
+            print("Woops! You Have Entered Letter That Is Already Used\n")
+            lives = lives - 1
+            print("Penalty! Lives Decrease By 1, Remains : " + str(lives) + "\n")
+            drawStickMan(8 - lives)
+            continue
+        if guess not in actualWordToGuess:
+            print("Woops! You Have Entered Wrong Guess\n")
+            lives = lives - 1
+            print("Penalty! Lives Decrease By 1, Remains : " + str(lives) + "\n")
+            drawStickMan(8 - lives)
+            continue
+        if guess.lower() in actualWordToGuess:
+            print("YES! You Have Entered Correct Guess\n")
+            drawStickMan(8 - lives)
+
+        usedLetters = usedLetters + guess
+        get_new_word_to_guess(actualWordToGuess, wordToGuess, guess)
+
+def print_hello():
     initialText = "#########################################\n" \
                   "# Hello Hangman Game Is About To Begin! #\n" \
                   "#     Guesses Should Be Characters!     #\n" \
                   "#        Type 'stop' To End Game!       #\n" \
                   "#########################################\n"
     print(initialText)
-    randomWords = RandomWords()
-    terminateFlag = 0
 
-    while not terminateFlag:
-        lives = 8
-        usedLetters = ""
-        actualWordToGuess = ""
-        while len(actualWordToGuess) < 4:
-            try:
-                actualWordToGuess = randomWords.get_random_word()
-            except BaseException:
-                continue
-        actualWordToGuess = actualWordToGuess.lower()
-        wordToGuess = ""
-        for x in range(len(actualWordToGuess)):
-            wordToGuess = wordToGuess + "_"
-        while True:
-            if lives == 0:
-                print("You Lost!\n")
-                break
-            if actualWordToGuess == wordToGuess:
-                print("You Won!\n")
-                break
-            print("Word To Guess Looks Like This : " + wordToGuess + "\n")
-            guess = input("Enter Your Guess : ")
-            print("\n")
-            guess = guess.strip()
-            guess = guess.lower()
-            if guess == "stop":
-                print("You Stopped Playing Hangman!\n")
-                break
-            if len(guess) == 0:
-                print("Woops! You Have Not Entered Anything\n")
-                lives = lives - 1
-                print("Penalty! Lives Decrease By 1, Remains : " + str(lives) + "\n")
-                drawStickMan(8 - lives)
-                continue
-            if len(guess) > 1:
-                print("Woops! You Have Entered Input Longer Than Character Size\n")
-                lives = lives - 1
-                print("Penalty! Lives Decrease By 1, Remains : " + str(lives) + "\n")
-                drawStickMan(8 - lives)
-                continue
-            if guess in usedLetters:
-                print("Woops! You Have Entered Letter That Is Already Used\n")
-                lives = lives - 1
-                print("Penalty! Lives Decrease By 1, Remains : " + str(lives) + "\n")
-                drawStickMan(8 - lives)
-                continue
-            if guess not in actualWordToGuess:
-                print("Woops! You Have Entered Wrong Guess\n")
-                lives = lives - 1
-                print("Penalty! Lives Decrease By 1, Remains : " + str(lives) + "\n")
-                drawStickMan(8 - lives)
-                continue
-            if guess.lower() in actualWordToGuess:
-                print("YES! You Have Entered Correct Guess\n")
-                drawStickMan(8 - lives)
-
-            usedLetters = usedLetters + guess
-            newWordToGuess = ""
-            for position in range(len(actualWordToGuess)):
-                if actualWordToGuess[position] == guess:
-                    newWordToGuess = newWordToGuess + guess
-                else:
-                    newWordToGuess = newWordToGuess + wordToGuess[position]
-            wordToGuess = newWordToGuess
-
-        print("Word To Guess Was : " + actualWordToGuess.upper())
-        terminateFlag = continueOrNot()
-
+def print_goodbye():
     goodByeText = "#########################################\n" \
                   "#               Farewell!               #\n" \
                   "#       May The Force Be With You!      #\n" \
                   "#########################################\n"
     print(goodByeText)
+
+def get_new_word_to_guess(actualWordToGuess, wordToGuess, guess):
+    newWordToGuess = ""
+    for position in range(len(actualWordToGuess)):
+        if actualWordToGuess[position] == guess:
+            newWordToGuess = newWordToGuess + guess
+        else:
+            newWordToGuess = newWordToGuess + wordToGuess[position]
+    wordToGuess = newWordToGuess
+
+def get_word_to_guess():
+    randomWords = RandomWords()
+    actualWordToGuess = ""
+    while len(actualWordToGuess) < 4:
+        try:
+            actualWordToGuess = randomWords.get_random_word()
+        except BaseException:
+            continue
+    return  actualWordToGuess.lower()
 
 
 def continueOrNot():
