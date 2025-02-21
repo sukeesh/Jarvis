c9b9cd587dfcf6349d275ca3b18b149212cd5129
Yusuf Demir <139581075+YusufDemir1210@users.noreply.github.com>
2025-02-21 12:36:08 +0100

    [feat] assemblyToHex tests (#17)
    * [feat] added test for valid I-type instruction (#16)
    
    * [feat] added test for valid R-type instruction (#16)
    
    * [feat] added test for invalid assembly command (#16)
    
    * [feat] added test for too large immediate (#16)
    
    * [fix] fixed the name of the file (#16)

---
+++ jarviscli/tests/test_assemblyToHex.py
@@ -0,0 +1,56 @@
+import unittest
+from tests import PluginTest  
+from plugins import mips_conv
+
+
+class AssemblyToHexTest(PluginTest):
+    def setUp(self):
+        self.test = self.load_plugin(mips_conv.MipsConverter)
+
+    def test_valid_addi(self):
+        """
+        Test conversion of a valid I-type instruction.
+        The final output should include a hex conversion message.
+        """
+        TEST_STRING = "ADDI $t2, $t1, 0x12"
+        self.test.run(TEST_STRING)
+        # We expect the final message to begin with "Statement in Hex: 0x"
+        expected_prefix = "Statement in Hex: 0x"
+        output = self.history_say().last_text()
+        # Compare only the prefix since the complete hex value depends on internal data.
+        self.assertEqual(output[:len(expected_prefix)], expected_prefix)
+
+    def test_valid_sll(self):
+        """
+        Test conversion of a valid R-type instruction.
+        The final output should include a hex conversion message.
+        """
+        TEST_STRING = "SLL $t2, $t1, 2"
+        self.test.run(TEST_STRING)
+        expected_prefix = "Statement in Hex: 0x"
+        output = self.history_say().last_text()
+        self.assertEqual(output[:len(expected_prefix)], expected_prefix)
+
+    def test_invalid_command(self):
+        """
+        Test conversion with an invalid assembly command.
+        The output should indicate that no such command exists.
+        """
+        TEST_STRING = "FOO $t0, $t1, 0x1"
+        self.test.run(TEST_STRING)
+        expected_output = "NO SUCH COMMAND IN ASSEMBLY"
+        self.assertEqual(self.history_say().last_text(), expected_output)
+
+    def test_immediate_too_large(self):
+        """
+        Test conversion with an immediate value that is too large.
+        The output should indicate that the immediate is too large.
+        """
+        TEST_STRING = "ADDI $t2, $t1, 70000"
+        self.test.run(TEST_STRING)
+        expected_output = "Immediate is too large"
+        self.assertEqual(self.history_say().last_text(), expected_output)
+
+
+if __name__ == '__main__':
+    unittest.main()
