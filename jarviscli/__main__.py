0735dea25464b8d758c2659d37be77cba0530020
Lorena Sierra <ls.rys.24@gmail.com>
2025-11-03 12:24:12 -0500

    Fix: bootstrap & CLI entrypoint; add __main__ for python -m jarviscli

--- jarviscli/__main__.py
+++ jarviscli/__main__.py
@@ -1,24 +1,8 @@
-# -*- coding: utf-8 -*-
-import Jarvis
-import colorama
-import sys
-
-
-def check_python_version():
-    return sys.version_info[0] == 3
-
+# Entry point for `python -m jarviscli`
+from .Jarvis import Jarvis
 
 def main():
-    # enable color on windows
-    colorama.init()
-    # start Jarvis
-    jarvis = Jarvis.Jarvis()
-    command = " ".join(sys.argv[1:]).strip()
-    jarvis.executor(command)
+    Jarvis().start()
 
-
-if __name__ == '__main__':
-    if check_python_version():
-        main()
-    else:
-        print("Sorry! Only Python 3 supported.")
+if __name__ == "__main__":
+    main()
